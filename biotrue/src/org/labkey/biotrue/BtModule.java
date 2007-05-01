e451749939997bf2eec8ba0ea58b4179439ea56b
Karl Lum <klum@labkey.com>
2007-05-01 02:33:21 +0000

    3311 : BtModule should verify schema 3298 : Saved grid filter gets used when switching to a different issues list set show charts button off by default for query views

--- biotrue/src/org/labkey/biotrue/BtModule.java
+++ biotrue/src/org/labkey/biotrue/BtModule.java
@@ -1,16 +1,19 @@
 package org.labkey.biotrue;
 
+import org.labkey.api.data.DbSchema;
 import org.labkey.api.module.DefaultModule;
 import org.labkey.api.module.ModuleContext;
 import org.labkey.api.query.DefaultSchema;
+import org.labkey.api.util.PageFlowUtil;
 import org.labkey.biotrue.controllers.BtController;
 import org.labkey.biotrue.controllers.BtOverviewWebPart;
+import org.labkey.biotrue.datamodel.BtManager;
 import org.labkey.biotrue.query.BtSchema;
-import org.labkey.biotrue.task.ScheduledTask;
 import org.labkey.biotrue.task.BtThreadPool;
+import org.labkey.biotrue.task.ScheduledTask;
 
-import java.util.Set;
 import java.util.Collections;
+import java.util.Set;
 
 public class BtModule extends DefaultModule
 {
@@ -25,6 +28,11 @@ public class BtModule extends DefaultModule
     public Set<String> getSchemaNames()
     {
         return Collections.singleton("biotrue");
+    }
+
+    public Set<DbSchema> getSchemasToTest()
+    {
+        return PageFlowUtil.set(BtManager.get().getSchema());
     }
 
     public void startup(ModuleContext moduleContext)
