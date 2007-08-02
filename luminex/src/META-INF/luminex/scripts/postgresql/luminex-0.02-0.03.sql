022126f5fc0efafd0178404d73e29db7b405bdeb
Josh Eckels <jeckels@labkey.com>
2007-08-02 17:38:15 +0000

    Fix up scripts - drop tables instead of deleting rows.

--- luminex/src/META-INF/luminex/scripts/postgresql/luminex-0.02-0.03.sql
+++ luminex/src/META-INF/luminex/scripts/postgresql/luminex-0.02-0.03.sql
@@ -1,6 +1,50 @@
-DELETE FROM luminex.DataRow;
-DELETE FROM luminex.Analyte;
+DROP TABLE luminex.DataRow;
+DROP TABLE luminex.Analyte;
 
-ALTER TABLE luminex.Analyte
-    ADD COLUMN MinStandardRecovery INT,
-    ADD COLUMN MaxStandardRecovery INT;
+CREATE TABLE luminex.Analyte
+(
+	RowId SERIAL NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    DataId INT NOT NULL,
+    FitProb REAL,
+    ResVar REAL,
+    RegressionType VARCHAR(100),
+    StdCurve VARCHAR(255),
+    MinStandardRecovery INT NOT NULL,
+    MaxStandardRecovery INT NOT NULL,
+
+	CONSTRAINT PK_Luminex_Analyte PRIMARY KEY (RowId),
+	CONSTRAINT FK_LuminexAnalyte_DataId FOREIGN KEY (DataId) REFERENCES exp.Data(RowId)
+);
+
+CREATE INDEX IX_LuminexAnalyte_DataId ON luminex.Analyte (DataId);
+
+CREATE TABLE luminex.DataRow
+(
+	RowId SERIAL NOT NULL,
+    DataId INT NOT NULL,
+    AnalyteId INT NOT NULL,
+    Type VARCHAR(10),
+    Well VARCHAR(50),
+    Outlier BOOLEAN,
+    Description VARCHAR(50),
+    FI REAL,
+    FIBackground REAL,
+    StdDev REAL,
+    PercentCV REAL,
+    ObsConcString VARCHAR(20),
+    ObsConc REAL,
+    ObsConcOORIndicator VARCHAR(10),
+    ExpConc REAL,
+    ObsOverExp REAL,
+    ConcInRangeString VARCHAR(20),
+    ConcInRange REAL,
+    ConcInRangeOORIndicator VARCHAR(10),
+
+	CONSTRAINT PK_Luminex_DataRow PRIMARY KEY (RowId),
+	CONSTRAINT FK_LuminexDataRow_DataId FOREIGN KEY (DataId) REFERENCES exp.Data(RowId),
+	CONSTRAINT FK_LuminexDataRow_AnalyteId FOREIGN KEY (DataId) REFERENCES exp.Data(RowId)
+);
+
+CREATE INDEX IX_LuminexDataRow_DataId ON luminex.DataRow (DataId);
+CREATE INDEX IX_LuminexDataRow_AnalyteId ON luminex.DataRow (AnalyteId);
