fe284738c2e542abda282b75726001b2e81bf104
Karl Lum <klum@labkey.com>
2007-10-10 19:51:16 +0000

    4154 : Meaningless caBIG checkbox on Customize Folder page 3291 : Modules should be able to specify 0 - n tabs
    Remove cabig and audit modules from the customize folder view. Allow modules to display container-specific tab labels.

--- caBIG/src/org/labkey/cabig/caBIGModule.java
+++ caBIG/src/org/labkey/cabig/caBIGModule.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.labkey.api.data.DbSchema;
 import org.labkey.api.module.DefaultModule;
 import org.labkey.api.module.ModuleContext;
+import org.labkey.api.module.Module;
 import org.labkey.api.security.SecurityManager;
 import org.labkey.api.util.PageFlowUtil;
 
@@ -36,6 +37,11 @@ public class caBIGModule extends DefaultModule
     public Set<DbSchema> getSchemasToTest()
     {
         return PageFlowUtil.set(caBIGSchema.getInstance().getSchema());
+    }
+
+    public TabDisplayMode getTabDisplayMode()
+    {
+        return TabDisplayMode.DISPLAY_NEVER;
     }
 
     @Override
