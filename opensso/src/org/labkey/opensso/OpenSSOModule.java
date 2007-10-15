514f05bdec8464abd0df8d9435cd6a0982329c51
Adam Rauch <adam@labkey.com>
2007-10-15 20:26:36 +0000

    Migrate all authentication to new AuthenticationProvider interface.  Lots of changes for OpenSSO integration.
    4192 : CAVD Single sign-on (WS-Federation)

--- opensso/src/org/labkey/opensso/OpenSSOModule.java
+++ opensso/src/org/labkey/opensso/OpenSSOModule.java
@@ -2,10 +2,14 @@ package org.labkey.opensso;
 
 import org.labkey.api.module.DefaultModule;
 import org.labkey.api.module.ModuleContext;
+import org.labkey.api.security.AuthenticationManager;
+import org.labkey.api.security.AuthenticationProvider;
+import org.apache.log4j.Logger;
 
 public class OpenSSOModule extends DefaultModule
 {
     public static final String NAME = "OpenSSO";
+    private static Logger _log = Logger.getLogger(OpenSSOModule.class);
 
     public OpenSSOModule()
     {
@@ -16,5 +20,16 @@ public class OpenSSOModule extends DefaultModule
     public void startup(ModuleContext moduleContext)
     {
         super.startup(moduleContext);
+
+        // TODO: Remove try/catch... but for now, ignore exceptions until OpenSSO integration is complete
+        try
+        {
+            AuthenticationProvider opensso = new OpenSSOProvider();
+            AuthenticationManager.registerProvider(opensso);
+        }
+        catch (Exception e)
+        {
+            _log.debug("OpenSSO initialization failure", e);
+        }
     }
 }
--- opensso/src/org/labkey/opensso/OpenSSOProvider.java
+++ opensso/src/org/labkey/opensso/OpenSSOProvider.java
@@ -1,21 +1,79 @@
 package org.labkey.opensso;
 
+import com.iplanet.am.util.SystemProperties;
+import com.iplanet.sso.SSOException;
+import com.iplanet.sso.SSOToken;
+import com.iplanet.sso.SSOTokenManager;
+import org.apache.log4j.Logger;
 import org.labkey.api.security.AuthenticationProvider;
-import org.labkey.api.security.User;
+import org.labkey.api.security.ValidEmail;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * User: adam
  * Date: Oct 10, 2007
  * Time: 7:01:10 PM
  */
-public class OpenSSOProvider implements AuthenticationProvider
+public class OpenSSOProvider implements AuthenticationProvider.RequestAuthenticationProvider
 {
-    public User authenticate(HttpServletRequest request, HttpServletResponse response)
+    private static final Logger _log = Logger.getLogger(OpenSSOProvider.class);
+
+    public OpenSSOProvider()
     {
-        // Look for opensso cookie... if not, look for opensso param... if not, return null
+        addProps("AMConfig.properties");
+
+        // TODO: Do we really need all these?
+        addProps("amAuth.properties");
+        addProps("amAuthContext.properties");
+        addProps("amIdRepo.properties");
+        addProps("amNaming.properties");
+        addProps("amProfile.properties");
+        addProps("amSecurity.properties");
+        addProps("amSession.properties");
+        addProps("amSSOProvider.properties");
+        addProps("amUtilMsgs.properties");
+        addProps("clientDefault.properties");
+    }
+
+    private void addProps(String filename)
+    {
+        try
+        {
+            Properties props = new Properties();
+            props.load(OpenSSOManager.class.getResourceAsStream(filename));
+            SystemProperties.initializeProperties(props);
+        }
+        catch (IOException e)
+        {
+            _log.error("Exception initializing OpenSSO properties", e);
+        }
+    }
+
+    public ValidEmail authenticate(HttpServletRequest request, HttpServletResponse response) throws ValidEmail.InvalidEmailException
+    {
+        // TODO: If no valid opensso cookie look for opensso param and automatically redirect
+        // ... if not, return null, and rely on login screen to present link to opensso
+
+        try
+        {
+            SSOTokenManager manager = SSOTokenManager.getInstance();
+            SSOToken token = manager.createSSOToken(request);
+            if (SSOTokenManager.getInstance().isValidToken(token))
+            {
+                String principalName = token.getPrincipal().getName();
+                int i = principalName.indexOf(',');
+                String email = principalName.substring(3, i);
+                return new ValidEmail(email);
+            }
+        }
+        catch (SSOException e)
+        {
+            // Ignore this -- opensso token is either expired or doesn't exist
+        }
 
         return null;
     }
