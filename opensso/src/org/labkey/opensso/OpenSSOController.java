07856d4fb93754fa078152876f999662476a275f
Adam Rauch <adam@labkey.com>
2007-10-31 05:37:42 +0000

    Ability to add logos that link to external authentication providers.  Each logo appears (if corresponding authentication provider is enabled) next to Sign In link in header and on login page.
    Remove hard-coded link to OpenSSO.  Suppress all "sign in" links while on login page.
    
    Fix naughty attachments junit test that's been deleting all attachments in root container (including logo + favicon).

--- opensso/src/org/labkey/opensso/OpenSSOController.java
+++ opensso/src/org/labkey/opensso/OpenSSOController.java
@@ -1,9 +1,10 @@
 package org.labkey.opensso;
 
 import org.labkey.api.action.FormViewAction;
-import org.labkey.api.action.SpringActionController;
 import org.labkey.api.action.SimpleViewAction;
+import org.labkey.api.action.SpringActionController;
 import org.labkey.api.security.ACL;
+import org.labkey.api.security.AuthenticationManager;
 import org.labkey.api.security.RequiresPermission;
 import org.labkey.api.view.JspView;
 import org.labkey.api.view.NavTree;
@@ -88,6 +89,7 @@ public class OpenSSOController extends SpringActionController
         public ModelAndView getView(ConfigProperties form, BindException errors) throws Exception
         {
             form.props = OpenSSOManager.get().getSystemSettings();
+            form.authLogoUrl = getPickAuthLogoUrl(getViewContext().getViewURLHelper());
             return new JspView<ConfigProperties>("/org/labkey/opensso/view/currentSettings.jsp", form);
         }
 
@@ -102,6 +104,7 @@ public class OpenSSOController extends SpringActionController
     {
         public String returnUrl;
         public Map<String, String> props;
+        public ViewURLHelper authLogoUrl;
 
         public String getReturnUrl()
         {
@@ -112,5 +115,20 @@ public class OpenSSOController extends SpringActionController
         {
             this.returnUrl = returnUrl;
         }
+    }
+
+
+    public static ViewURLHelper getPickAuthLogoUrl(ViewURLHelper returnUrl)
+    {
+        ViewURLHelper url = getUrl("pickAuthLogo.view", returnUrl);
+        url.addParameter("name", "OpenSSO");   // TODO: Add this as getting in action instead? 
+        return url;
+    }
+
+
+    @RequiresPermission(ACL.PERM_ADMIN)
+    public class PickAuthLogoAction extends AuthenticationManager.PickAuthLogoAction
+    {
+
     }
 }
--- opensso/src/org/labkey/opensso/view/currentSettings.jsp
+++ opensso/src/org/labkey/opensso/view/currentSettings.jsp
@@ -1,14 +1,16 @@
 <%@ page import="org.labkey.api.util.PageFlowUtil" %>
 <%@ page import="org.labkey.api.view.HttpView" %>
 <%@ page import="org.labkey.api.view.JspView" %>
-<%@ page import="org.labkey.opensso.OpenSSOController.ConfigProperties" %>
 <%@ page import="org.labkey.opensso.OpenSSOController" %>
+<%@ page import="org.labkey.opensso.OpenSSOController.ConfigProperties" %>
 <%@ page extends="org.labkey.api.jsp.JspBase" %>
 <%
     JspView<ConfigProperties> me = (JspView<ConfigProperties>)HttpView.currentView();
     ConfigProperties bean = me.getModelBean();
 %>
-<table><%
+<table>
+    <tr><td colspan="2"><a href="<%=bean.authLogoUrl%>">Pick a logo and link to use for OpenSSO sign in</a></td></tr>
+    <tr><td colspan="2">&nbsp;</td></tr><%
 
     for (String key : bean.props.keySet())
     {
