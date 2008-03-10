ed864bef67b32a05b9ba344d5a4fa7270c006990
Karl Lum <klum@labkey.com>
2008-03-10 19:47:23 +0000

    5350 : javascript error navigating away from R reports 5323 : Need project-visible function definition scripts usable in subfolders 5293 : Reloading R results view when editing fails to open output file 5145 : ELISpot needs a default template

--- elispot/src/org/labkey/elispot/ElispotPlateTypeHandler.java
+++ elispot/src/org/labkey/elispot/ElispotPlateTypeHandler.java
@@ -22,12 +22,51 @@ public class ElispotPlateTypeHandler implements PlateTypeHandler
 
     public List<String> getTemplateTypes()
     {
-        return new ArrayList<String>();
+        List<String> names = new ArrayList<String>();
+        names.add("Default");
+        return names;
     }
 
     public PlateTemplate createPlate(String templateTypeName, Container container) throws SQLException
     {
-        return PlateService.get().createPlateTemplate(container, getAssayType());
+        PlateTemplate template = PlateService.get().createPlateTemplate(container, getAssayType());
+
+        for (int sample = 0; sample < 4; sample++)
+        {
+            int row = sample * 2;
+            // create the overall specimen group, consisting of two adjacent rows:
+            template.addWellGroup("Specimen " + (sample + 1), WellGroup.Type.SPECIMEN,
+                    PlateService.get().createPosition(container, row, 0),
+                    PlateService.get().createPosition(container, row+1, template.getColumns() - 1));
+        }
+
+        if (templateTypeName != null && templateTypeName.equalsIgnoreCase("Default"))
+        {
+            // populate the antigen groups
+            for (int antigen = 0; antigen < 4; antigen++)
+            {
+                List<Position> position1 = new ArrayList<Position>();
+                List<Position> position2 = new ArrayList<Position>();
+
+                for (int sample = 0; sample < 4; sample++)
+                {
+                    int row = sample * 2;
+                    int col = antigen * 3;
+
+                    position1.add(template.getPosition(row, col));
+                    position1.add(template.getPosition(row, col + 1));
+                    position1.add(template.getPosition(row, col + 2));
+
+                    position2.add(template.getPosition(row + 1, col));
+                    position2.add(template.getPosition(row + 1, col + 1));
+                    position2.add(template.getPosition(row + 1, col + 2));
+                }
+                template.addWellGroup("Antigen " + (antigen*2 + 1), WellGroup.Type.ANTIGEN, position1);
+                template.addWellGroup("Antigen " + (antigen*2 + 2), WellGroup.Type.ANTIGEN, position2);
+            }
+        }
+
+        return template;
     }
 
     public WellGroup.Type[] getWellGroupTypes()
