5b05072f192aff112509d762e1bd306bbe10d81c
Adam Rauch <adam@labkey.com>
2008-05-13 21:57:03 +0000

    Add/fix copyright notices on all Java files.

--- ms2/src/org/labkey/ms2/pipeline/DescribeRunPage.java
+++ ms2/src/org/labkey/ms2/pipeline/DescribeRunPage.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2005-2008 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.labkey.ms2.pipeline;
 
 import org.fhcrc.cpas.exp.xml.*;
@@ -21,7 +36,6 @@ import java.util.Iterator;
 import java.sql.SQLException;
 
 /**
- * Copyright (C) 2004 Fred Hutchinson Cancer Research Center. All Rights Reserved.
  * User: migra
  * Date: Dec 8, 2005
  * Time: 10:24:01 AM
