9181b0e5c676918912f9d0e8f34b1e9f77b52562
Adam Rauch <adam@labkey.com>
2008-05-19 19:21:04 +0000

    Update copyrights in all .jsp, .sql, and .gm files.

--- ms2/src/META-INF/ms2/scripts/postgresql/obsolete/ms2-1.32-1.33.sql
+++ ms2/src/META-INF/ms2/scripts/postgresql/obsolete/ms2-1.32-1.33.sql
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2006-2008 Fred Hutchinson Cancer Research Center
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 -- Relax contraints on quantitation result columns; q3 does not generate string representations of ratios.
 ALTER TABLE ms2.Quantitation ALTER COLUMN Ratio DROP NOT NULL;
 ALTER TABLE ms2.Quantitation ALTER COLUMN Heavy2lightRatio DROP NOT NULL;
