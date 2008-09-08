672a1ec09edf444529fcdc368d3c03b867203072
Matthew Bellew <mbellew@labkey.com>
2008-09-08 22:45:01 +0000

    BulkUpdateKeywordsAction

--- flow/src/org/labkey/flow/controllers/well/WellController.java
+++ flow/src/org/labkey/flow/controllers/well/WellController.java
@@ -26,7 +26,12 @@ import org.labkey.api.security.ACL;
 import org.labkey.api.security.RequiresPermission;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.util.URIUtil;
+import org.labkey.api.util.ResultSetUtil;
 import org.labkey.api.view.*;
+import org.labkey.api.data.SQLFragment;
+import org.labkey.api.data.Table;
+import org.labkey.api.data.DbSchema;
+import org.labkey.api.data.RuntimeSQLException;
 import org.labkey.flow.analysis.web.FCSAnalyzer;
 import org.labkey.flow.analysis.web.FCSViewer;
 import org.labkey.flow.analysis.web.GraphSpec;
@@ -36,7 +41,9 @@ import org.labkey.flow.controllers.FlowParam;
 import org.labkey.flow.controllers.SpringFlowController;
 import org.labkey.flow.data.*;
 import org.labkey.flow.persist.FlowManager;
+import org.labkey.flow.persist.ObjectType;
 import org.labkey.flow.script.FlowAnalyzer;
+import org.labkey.flow.query.AttributeCache;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
@@ -47,6 +54,9 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class WellController extends SpringFlowController<WellController.Action>
 {
@@ -335,6 +345,186 @@ public class WellController extends SpringFlowController<WellController.Action>
         }
     }
 
+
+    @RequiresPermission(ACL.PERM_ADMIN)
+    public class BulkUpdateKeywordsAction extends FormViewAction<UpdateKeywordsForm>
+    {
+        Integer keywordid = null;
+        int updated = -1;
+
+        public void validateCommand(UpdateKeywordsForm form, Errors errors)
+        {
+            if (null == form.keyword)
+                errors.rejectValue("keyword", ERROR_REQUIRED);
+            if (null == form.from)
+                form.from = new String[0];
+            if (null == form.to)
+                form.to = new String[0];
+            if (form.from.length != form.to.length)
+            {
+                errors.reject("from length and to length do not match");
+            }
+            else
+            {
+                for (int i=0 ; i<form.from.length ; i++)
+                {
+                    form.from[i] = StringUtils.trimToNull(form.from[i]);
+                    form.to[i] = StringUtils.trimToNull(form.to[i]);
+                    if ((null == form.from[i]) != (null == form.to[i]))
+                        errors.reject(ERROR_MSG, "Empty value not allowed");
+                }
+            }
+
+            SQLFragment sql = new SQLFragment("SELECT rowid FROM flow.attribute WHERE name=?", form.keyword);
+            try
+            {
+                keywordid = Table.executeSingleton(DbSchema.get("flow"), sql.getSQL(), sql.getParams().toArray(), Integer.class);
+                if (null == keywordid)
+                    errors.rejectValue("keyword", ERROR_MSG, "keyword not found: " + form.keyword);
+            }
+            catch (SQLException x)
+            {
+                throw new RuntimeSQLException(x);
+            }
+        }
+
+        public ModelAndView getView(UpdateKeywordsForm form, boolean reshow, BindException errors) throws Exception
+        {
+            return new JspView<UpdateKeywordsForm>(WellController.class, "bulkUpdate.jsp", form, errors);
+            //return new GroovyView<UpdateKeywordsForm>("/org/labkey/flow/controllers/well/bulkUpdate.jsp", form);
+        }
+
+        public NavTree appendNavTrail(NavTree root)
+        {
+            return root;
+        }
+
+        public boolean handlePost(UpdateKeywordsForm form, BindException errors) throws Exception
+        {
+            SQLFragment update = new SQLFragment();
+            update.append("UPDATE flow.keyword SET value = CASE\n");
+            for (int i=0 ; i<form.from.length ; i++)
+            {
+                if (form.from[i] != null && form.to[i] != null)
+                {
+                    update.append("  WHEN value=? THEN ?\n");
+                    update.add(form.from[i]);
+                    update.add(form.to[i]);
+                }
+            }
+            update.append("  ELSE value END\n");
+            update.append("WHERE objectid IN (SELECT O.rowid from flow.object O where O.container=? and O.typeid=?) AND keywordid=?");
+            update.add(getViewContext().getContainer());
+            update.add(ObjectType.fcsKeywords.getTypeId());
+            update.add(keywordid);
+            update.append("  AND value IN (");
+            String param = "?";
+            for (int i=0 ; i<form.from.length ; i++)
+            {
+                if (null != form.from[i])
+                {
+                    update.append(param);
+                    update.add(form.from[i]);
+                    param = ",?";
+                }
+            }
+            update.append(")");
+
+            updated = Table.execute(DbSchema.get("flow"), update);
+
+            form.message = "" + updated + " values updated";
+            // CONSIDER handle nulls (requires INSERT and DELETE)
+            return true;
+        }
+
+        public ActionURL getSuccessURL(UpdateKeywordsForm form)
+        {
+            return null;
+        }
+
+        public ModelAndView getSuccessView(UpdateKeywordsForm form)
+        {
+            return new MessageView(form.message, "bulkUpdateKeywords.view");
+        }
+    }
+
+    public static class MessageView extends HtmlView
+    {
+        MessageView(String message, String href)
+        {
+            super(null);
+            StringBuilder sb = new StringBuilder();
+            sb.append("<span style='color:green;'>");
+            sb.append(PageFlowUtil.filter(message));
+            sb.append("</span><br>");
+            sb.append(PageFlowUtil.generateButton("OK",href));
+            setHtml(sb.toString());
+        }
+    }
+
+    public static class UpdateKeywordsForm
+    {
+        String returnURL = "begin.view";
+        public String keyword = null;
+        public String[] from = new String[0];
+        public String[] to = new String[0];
+        public String message;
+
+        public void setKeyword(String keyword)
+        {
+            this.keyword = keyword;
+        }
+
+        public String getKeyword()
+        {
+            return keyword;
+        }
+
+        public void setFrom(String[] from)
+        {
+            this.from = from;
+        }
+
+        public void setTo(String[] to)
+        {
+            this.to = to;
+        }
+
+        public void setReturnURL(String url)
+        {
+            this.returnURL = url;
+        }
+
+        public TreeSet<String> getKeywords(ViewContext context)
+        {
+            Map<String,Integer> map = AttributeCache.KEYWORDS.getAttrValues(context.getContainer(), null);
+            return new TreeSet<String>(map.keySet());
+        }
+
+        public TreeSet<String> getValues(ViewContext context, String keyword)
+        {
+            ResultSet rs = null;
+            try
+            {
+                rs = Table.executeQuery(DbSchema.get("flow"),
+                        "SELECT DISTINCT value FROM flow.keyword WHERE keywordid = (SELECT rowid FROM flow.attribute WHERE name=?)", new Object[]{keyword});
+                TreeSet<String> set = new TreeSet<String>();
+                while (rs.next())
+                    set.add(rs.getString(1));
+                return set;
+            }
+            catch (SQLException x)
+            {
+                throw new RuntimeSQLException(x);
+            }
+            finally
+            {
+                ResultSetUtil.close(rs);
+            }
+        }
+    }
+
+    
     static abstract public class Page extends FlowPage
     {
         private FlowRun _run;
---
+++ flow/src/org/labkey/flow/controllers/well/bulkUpdate.jsp
@@ -0,0 +1,123 @@
+<%@ page import="org.labkey.api.util.PageFlowUtil" %>
+<%@ page import="org.labkey.flow.controllers.well.WellController" %>
+<%@ page import="org.labkey.api.view.HttpView" %>
+<%@ page import="org.labkey.api.view.ViewContext" %>
+<%@ page import="org.apache.commons.lang.StringUtils" %>
+<%@ taglib prefix="labkey" uri="http://www.labkey.org/taglib" %>
+
+<%
+WellController.UpdateKeywordsForm form = (WellController.UpdateKeywordsForm)HttpView.currentModel();
+ViewContext context = HttpView.currentContext();
+
+%>
+<script type="text/javascript">
+    LABKEY.requiresExtJs();
+</script>
+<span style="color:green;"><%=PageFlowUtil.filter(form.message)%></span><br>
+<labkey:errors />
+<div id="updateForm" />
+<script type="text/javascript">
+
+var keywords = [
+<%
+    String comma = "";
+    for (String kw : form.getKeywords(context))
+    {
+        out.print(comma);
+        out.print("[");
+        out.print(PageFlowUtil.jsString(kw));
+        out.print("]");
+        comma = ",";
+    }%>
+];
+var keywordStore = new Ext.data.SimpleStore({fields:['keyword'], data:keywords}) 
+var values = [
+<%
+    comma = "";
+    for (String v : form.getValues(context,form.keyword))
+    {
+        out.print(comma);
+        out.print("[");
+        out.print(PageFlowUtil.jsString(v));
+        out.print("]");
+        comma = ",";
+    }%>
+];
+var valuesStore = new Ext.data.SimpleStore({fields:['value'], data:values})
+
+var updateFormPanel;
+var keywordCombo;
+
+function keywordCombo_onSelect()
+{
+    var kw = keywordCombo.getValue();
+    window.location = '?keyword=' + kw;
+}
+
+function form_onSubmit()
+{
+    var form = updateFormPanel.getForm();
+    form.getEl().dom.action="bulkUpdateKeywords.post";
+    form.getEl().dom.submit();
+}
+
+Ext.onReady(function(){
+
+    Ext.QuickTips.init();
+
+    // turn on validation errors beside the field globally
+    Ext.form.Field.prototype.msgTarget = 'side';
+
+    updateFormPanel = new Ext.FormPanel({
+        
+        onSubmit: Ext.emptyFn, submit: form_onSubmit,
+
+        labelWidth: 75, // label settings here cascade unless overridden
+        url:'save-form.php',
+        frame:true,
+        title: 'Update Keywords',
+        bodyStyle:'padding:5px 5px 0',
+        width: 350,
+        defaults: {width: 230},
+        defaultType: 'textfield',
+
+        items:
+        [
+            keywordCombo = new Ext.form.ComboBox({
+                fieldLabel: 'Keyword',
+                name: 'keyword',
+                forceSelection: true,
+                value: <%=PageFlowUtil.jsString(form.keyword)%>,
+                displayField:'keyword',
+                typeAhead: true,
+                mode: 'local',
+                triggerAction: 'all',
+                store: keywordStore
+            })
+<% if (null != StringUtils.trimToNull(form.keyword)) {for (int i=0; i<4 ; i++) {            
+            %>,
+            {
+                xtype:'fieldset',
+                //title:'<%=i%>', collapsible:true, collapsed:<%=i==0?"false":"true"%>,
+                width: 330, autoHeight:true, defaults: {width: 210},
+                defaultType: 'textfield',
+                items:
+                [
+                    new Ext.form.ComboBox({fieldLabel:'Current Value', name:'from', forceSelection:false,
+                        lazyRender:true, displayField:'value', typeAhead:true, mode:'local', triggerAction:'all',
+                        store: valuesStore}),
+                    new Ext.form.ComboBox({fieldLabel:'New Value', name:'to', forceSelection:false,
+                        lazyRender:true, displayField:'value', typeAhead:true, mode:'local', triggerAction:'all',
+                        store: valuesStore})
+                ]
+            } <%}}%>
+        ],
+        buttons:[{text: 'Update', type:'submit', handler:function() {updateFormPanel.getForm().submit({url:"bulkUpdateKeywords.view"})}}]
+    });
+
+    keywordCombo.on('select', keywordCombo_onSelect);
+    
+    updateFormPanel.render('updateForm');
+});
+
+</script>
