048273a2c49bd99e22bf3435bdb61d11649ffc61
Josh Eckels <jeckels@labkey.com>
2009-04-15 00:00:32 +0000

    Add a BeginAction, remove an unused action

--- ms2/src/org/labkey/ms2/xarassay/XarAssayController.java
+++ ms2/src/org/labkey/ms2/xarassay/XarAssayController.java
@@ -16,34 +16,26 @@
 
 package org.labkey.ms2.xarassay;
 
-import org.labkey.api.action.FormViewAction;
+import org.labkey.api.action.SimpleRedirectAction;
 import org.labkey.api.action.SpringActionController;
-import org.labkey.api.data.Container;
-import org.labkey.api.exp.Lsid;
 import org.labkey.api.exp.api.ExpProtocol;
 import org.labkey.api.security.ACL;
 import org.labkey.api.security.RequiresPermission;
-import org.labkey.api.study.assay.AssayService;
-import org.labkey.api.study.assay.PipelineDataCollectorRedirectAction;
 import org.labkey.api.study.assay.AssayUrls;
-import org.labkey.api.view.*;
+import org.labkey.api.study.assay.PipelineDataCollectorRedirectAction;
 import org.labkey.api.util.PageFlowUtil;
+import org.labkey.api.view.ActionURL;
 import org.springframework.validation.BindException;
-import org.springframework.validation.Errors;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.io.FileFilter;
 import java.io.File;
+import java.io.FileFilter;
+import java.util.List;
 
 
 public class XarAssayController extends SpringActionController
 {
     private static final DefaultActionResolver _resolver =
-            new DefaultActionResolver(XarAssayController.class,
-                    XarAssayUploadAction.class, ChooseAssayAction.class);
+            new DefaultActionResolver(XarAssayController.class, XarAssayUploadAction.class);
 
 
     public XarAssayController()
@@ -52,6 +44,16 @@ public class XarAssayController extends SpringActionController
         setActionResolver(_resolver);
     }
 
+    @RequiresPermission(ACL.PERM_READ)
+    public class BeginAction extends SimpleRedirectAction
+    {
+        public ActionURL getRedirectURL(Object o) throws Exception
+        {
+            return getContainer().getStartURL(getViewContext());
+        }
+    }
+
+    @RequiresPermission(ACL.PERM_INSERT)
     public class UploadRedirectAction extends PipelineDataCollectorRedirectAction
     {
         protected FileFilter getFileFilter()
@@ -67,86 +69,6 @@ public class XarAssayController extends SpringActionController
         protected List<File> validateFiles(BindException errors, List<File> files)
         {
             return files;
-        }
-    }
-
-    /*
-    builds up a list of Assay providers that extend the XarAssay provider
-     */
-    @RequiresPermission(ACL.PERM_INSERT)
-    public class ChooseAssayAction extends FormViewAction<XarChooseAssayForm>
-    {
-        ActionURL _returnURL = null;
-
-        public ModelAndView getView(XarChooseAssayForm form, boolean reshow, BindException errors) throws Exception
-        {
-            return new JspView<XarChooseAssayForm>("/org/labkey/ms2/xarassay/view/chooseAssay.jsp",form) ;
-        }
-        public NavTree appendNavTrail(NavTree root)
-        {
-            root.addChild("Select Assay Type");
-            return root;
-        }
-
-        public void validateCommand(XarChooseAssayForm target, Errors errors)
-        {
-        }
-
-        public boolean handlePost(XarChooseAssayForm form, BindException errors) throws Exception
-        {
-            ViewContext ctx = getViewContext();
-            Container c = ctx.getContainer();
-            form.setPath(ctx.getRequest().getParameter("path"));
-            Integer protId = null;
-            try {
-                protId = new Integer(ctx.getRequest().getParameter("protId"));}
-            catch (NumberFormatException e) {}
-
-            if ((null != protId) && (null == form.getRowId()))
-                form.setRowId(protId);
-
-            if (null != form.getRowId())
-            {
-                // came from Assay definition, already know which Assay we want
-                ActionURL helper = form.getViewContext().getActionURL().clone();
-                helper.setAction(XarAssayUploadAction.class);
-                helper.addParameter("referer", "pipeline");
-                helper.addParameter("rowId", form.getRowId());
-
-                //TODO  encode?
-                if (null!= form.getPath())
-                    helper.addParameter("path", form.getPath());
-                HttpView.redirect(helper);
-            }
-
-            ArrayList<ExpProtocol> assayProtocols = new ArrayList<ExpProtocol>();
-
-            ActionURL runUploadLink = new ActionURL(XarAssayUploadAction.class, c );
-            runUploadLink.addParameter("path",form.getPath());
-            runUploadLink.addParameter("referer","pipeline");
-
-            Map<String, XarAssayProvider> mapMsBaseAssayProviders = XarAssayProvider.getMsBaseAssayProviders();
-
-            List<ExpProtocol> ap = AssayService.get().getAssayProtocols(c);
-            for (ExpProtocol p : ap)
-            {
-                Lsid lsid = new Lsid(p.getLSID());
-                if (mapMsBaseAssayProviders.containsKey(lsid.getNamespacePrefix()) )
-                {
-                    assayProtocols.add(p);
-                    runUploadLink.replaceParameter("rowId", Integer.toString(p.getRowId()));
-                    form.getLinks().put(p.getName(), runUploadLink.toString());
-                }
-            }
-            form.setAvailableProtocols(assayProtocols);
-
-            setReshow(false);
-            return true;
-        }
-
-        public ActionURL getSuccessURL(XarChooseAssayForm xarChooseAssayForm)
-        {
-            return null;  
         }
     }
 }
