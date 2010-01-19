ae12740eb7fef4e6680ce4dcd3c77e61306d1be8
Kevin Krouse <kevink@labkey.com>
2010-01-19 02:03:35 +0000

    viability: support navigation using arrow up/down on MultiValueInput field

--- viability/webapp/viability/MultiValueInput.js
+++ viability/webapp/viability/MultiValueInput.js
@@ -9,30 +9,97 @@ var MultiValueInput = function (fieldId, initialValues)
     var fields = [];
     var self = this;
 
+    // collect all MultiValueInput form inputs and return the index of the passed in input.
+    function collectInputs(input, inputs)
+    {
+        var form = input.form;
+        var len = form.elements.length;
+        var index = -1;
+        for (var i = 0; i < len; i++)
+        {
+            var node = form.elements[i];
+            if (node.className.indexOf('labkey-multi-value-input') != -1)
+            {
+                var count = inputs.push(node);
+                if (node == input)
+                    index = count-1;
+            }
+        }
+        return index;
+    }
+
+    // move focus down to next 'labkey-multi-value-input' field
+    function goDown(input)
+    {
+        var inputs = [];
+        var index = collectInputs(input, inputs);
+        if (index > -1 && index < inputs.length - 1)
+        {
+            var node = inputs[index+1];
+            if (node)
+                node.focus();
+        }
+    }
+
+    // move focus up to previous 'labkey-multi-value-input' field
+    function goUp(input)
+    {
+        var inputs = [];
+        var index = collectInputs(input, inputs);
+        if (index > 0 && index < inputs.length)
+        {
+            var node = inputs[index-1];
+            if (node)
+                node.focus();
+        }
+    }
+
     this.addInput = function (value, getfocus) {
         var field = new Ext.form.TextField({
             name: fieldId,
             editable: true,
             value: value,
             style: "display:block;font-family:monospace",
+            cls: "labkey-multi-value-input",
             listeners: {
                 'specialkey': function (f, e) {
                     var key = e.getKey();
-                    if ((key == e.TAB || key == e.ENTER) && !e.shiftKey)
+                    if ((key == e.TAB || key == e.ENTER || key == e.DOWN) && !e.shiftKey)
                     {
                         if (key == e.ENTER)
                             e.stopEvent();
+
                         var index = fields.indexOf(f);
                         if (index == fields.length - 1)
                         {
                             if (f.getValue()) {
+                                // if last field has a value, add a new field
                                 self.addInput('', true);
                             } else if (fields.length > 1) {
+                                // if last field is empty, remove it
                                 fields.pop();
-                                // defer destroy so focus is moved to next element
+                                // defer destroy so focus is moved to next element on TAB
                                 f.destroy.defer(10, f);
                             }
                         }
+
+                        if (key == e.DOWN)
+                            goDown(f.el.dom);
+                    }
+                    else if (key == e.UP)
+                    {
+                        var index = fields.indexOf(f);
+                        if (index == fields.length - 1)
+                        {
+                            if (!(f.getValue()) && fields.length > 1) {
+                                // if last field is empty, remove it
+                                fields.pop();
+                                // defer destroy so focus is moved to next element on TAB
+                                f.destroy.defer(10, f);
+                            }
+                        }
+
+                        goUp(f.el.dom);
                     }
                 }
             }
