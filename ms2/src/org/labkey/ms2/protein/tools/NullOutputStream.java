58ca249966681453533cfc616024db996d9041f9
Josh Eckels <jeckels@labkey.com>
2010-08-02 22:41:19 +0000

    10706 : Include details URL for study.StudyData
    Also, stop overriding TableInfo.getDetailsURL() in subclasses so that metadata overrides can change it. Stop supporting multiple DetailURLs now that query is smarter about including and resolving the required columns. The experiment module was the only real user and those usages have been updated.

--- ms2/src/org/labkey/ms2/protein/tools/NullOutputStream.java
+++ ms2/src/org/labkey/ms2/protein/tools/NullOutputStream.java
@@ -19,11 +19,8 @@ package org.labkey.ms2.protein.tools;
 import java.io.ByteArrayOutputStream;
 
 /**
- * Created by IntelliJ IDEA.
  * User: tholzman
  * Date: Oct 31, 2005
- * Time: 2:01:18 PM
- * To change this template use File | Settings | File Templates.
  */
 public class NullOutputStream extends ByteArrayOutputStream
 {
