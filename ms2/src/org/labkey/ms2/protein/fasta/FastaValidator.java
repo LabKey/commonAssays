3ceeafb012d47e945af2bd9bcd492557b7138aad
Adam Rauch <adam@labkey.com>
2010-11-13 01:51:56 +0000

    Consolidate and simplify CaseInsensitiveTreeMap; make public.
    More complete CaseInsensitiveHashSet implementation -- Sets.newCaseInsensitiveHashSet()
    
    Initial work to ask database server for reserved words instead of using hard-coded list.
    
    Check all sql scripts for "illegal" operations (e.g., unnamed primary keys, SET SEARCH_PATH on PostgreSQL) -- we used to exclude old scripts, but they've all been updated.

--- ms2/src/org/labkey/ms2/protein/fasta/FastaValidator.java
+++ ms2/src/org/labkey/ms2/protein/fasta/FastaValidator.java
@@ -15,7 +15,7 @@
  */
 package org.labkey.ms2.protein.fasta;
 
-import org.labkey.api.collections.CaseInsensitiveHashSet;
+import org.labkey.api.collections.Sets;
 
 import java.io.File;
 import java.text.DecimalFormat;
@@ -42,7 +42,7 @@ public class FastaValidator
 
     public List<String> validate()
     {
-        Set<String> proteinNames = new CaseInsensitiveHashSet(1000);
+        Set<String> proteinNames = Sets.newCaseInsensitiveHashSet(1000);
         List<String> errors = new ArrayList<String>();
         Format lineFormat = DecimalFormat.getIntegerInstance();
         ProteinFastaLoader curLoader = new ProteinFastaLoader(_fastaFile);
@@ -50,7 +50,7 @@ public class FastaValidator
         //noinspection ForLoopReplaceableByForEach
         for (ProteinFastaLoader.ProteinIterator proteinIterator = curLoader.iterator(); proteinIterator.hasNext();)
         {
-            Protein protein = (Protein)proteinIterator.next();
+            Protein protein = proteinIterator.next();
 
             // Creating a new string here is critical for handling large FASTA files -- see issue #6441 
             //noinspection RedundantStringConstructorCall
