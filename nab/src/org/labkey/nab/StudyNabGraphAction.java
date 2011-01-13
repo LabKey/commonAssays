78078e2f89772403808815151474739818de9847
Adam Rauch <adam@labkey.com>
2011-01-13 18:38:14 +0000

    Update licenses.

--- nab/src/org/labkey/nab/StudyNabGraphAction.java
+++ nab/src/org/labkey/nab/StudyNabGraphAction.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Copyright (c) 2010 LabKey Corporation
+ * Copyright (c) 2010-2011 LabKey Corporation
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
