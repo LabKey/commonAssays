a88603c6167e098eb82fcd3748ef1f4bc714b633
Kevin Krouse <kevink@labkey.com>
2011-02-17 23:47:13 +0000

    gauva ExpressPlus file format parsing

--- viability/src/org/labkey/viability/GuavaDataHandler.java
+++ viability/src/org/labkey/viability/GuavaDataHandler.java
@@ -45,6 +45,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -95,6 +96,7 @@ public class GuavaDataHandler extends ViabilityAssayDataHandler implements Trans
             {
                 reader = new BufferedReader(new FileReader(_dataFile));
                 boolean foundBlankLine = false;
+                Map<String, String> runHeaders = new HashMap<String, String>();
                 String[] groupHeaders = null;
                 String[] headers = null;
                 int count = 0;
@@ -109,18 +111,27 @@ public class GuavaDataHandler extends ViabilityAssayDataHandler implements Trans
                         {
                             foundBlankLine = true;
                         }
-                        else if (_runDomain != null)
+                        else
                         {
                             String firstCell = parts.length > 0 ? parts[0] : line;
                             String[] runMeta = firstCell.split(" - ", 2);
                             if (runMeta.length == 2 && runMeta[0].length() > 0 && runMeta[1].length() > 0)
                             {
-                                DomainProperty property = _runDomain.getPropertyByName(runMeta[0].trim());
-                                if (property != null)
+                                String runHeaderKey = runMeta[0].trim();
+                                String runHeaderValue = runMeta[1].trim();
+                                runHeaders.put(runHeaderKey, runHeaderValue);
+
+                                if (_runDomain != null)
                                 {
-                                    Object value = convert(property, runMeta[1].trim());
-                                    if (value != null)
-                                        _runData.put(property, value);
+                                    DomainProperty property = _runDomain.getPropertyByName(runHeaderKey);
+                                    if (property == null && runHeaderKey.equalsIgnoreCase("Current Date"))
+                                        property = _runDomain.getPropertyByName("Date");
+                                    if (property != null)
+                                    {
+                                        Object value = convert(property, runHeaderValue);
+                                        if (value != null)
+                                            _runData.put(property, value);
+                                    }
                                 }
                             }
                         }
@@ -150,86 +161,182 @@ public class GuavaDataHandler extends ViabilityAssayDataHandler implements Trans
                     count++;
                 }
 
-                if (groupHeaders == null || headers == null)
+                if (runHeaders.isEmpty() || groupHeaders == null || headers == null)
                     throw new ExperimentException("Failed to find header rows in guava file");
 
-                final int COL_SAMPLE_NUM = 0;
-                final int COL_SAMPLE_ID = 1;
-                final int COL_VIABLE = 11;
-                final int COL_TOTAL_VIABLE = 42;
-                final int COL_TOTAL_CELLS = 45;
+                String softwareName = runHeaders.get("Software Name");
+                if (softwareName == null)
+                    throw new ExperimentException("Expected 'Software Name' to appear in first lines of guava file");
 
-                ColumnDescriptor[] columns = new ColumnDescriptor[headers.length];
-                for (int i = 0; i < headers.length; i++)
-                {
-                    ColumnDescriptor cd = new ColumnDescriptor();
-                    String expectHeader = null;
-                    switch (i)
-                    {
-                        case COL_SAMPLE_NUM:
-                            cd.name = ViabilityAssayProvider.SAMPLE_NUM_PROPERTY_NAME;
-                            expectHeader = "Sample No.";
-                            cd.clazz = Integer.class;
-                            break;
-                        case COL_SAMPLE_ID:
-                            cd.name = ViabilityAssayProvider.POOL_ID_PROPERTY_NAME;
-                            expectHeader = "Sample ID";
-                            cd.clazz = String.class;
-                            break;
-                        case COL_VIABLE:
-                            cd.name = ViabilityAssayProvider.VIABILITY_PROPERTY_NAME;
-                            expectHeader = "Viable";
-                            cd.clazz = Double.class;
-                            cd.converter = new Converter ()
-                            {
-                                public Object convert(Class type, Object value)
-                                {
-                                    Double d = (Double)ConvertUtils.convert((String)value, Double.class);
-                                    if (d != null)
-                                        return d.doubleValue() / 100;
-                                    return null;
-                                }
-                            };
-                            break;
-                        case COL_TOTAL_VIABLE:
-                            cd.name = ViabilityAssayProvider.VIABLE_CELLS_PROPERTY_NAME;
-                            expectHeader = "Total Viable Cells in Original Sample";
-                            cd.clazz = Double.class;
-                            break;
-                        case COL_TOTAL_CELLS:
-                            cd.name = ViabilityAssayProvider.TOTAL_CELLS_PROPERTY_NAME;
-                            expectHeader = "Total Cells in Original Sample";
-                            cd.clazz = Double.class;
-                            break;
-                        default:
-                            cd.load = false;
-                    }
-
-                    if (expectHeader != null)
-                    {
-                        assert cd.load;
-                        if (headers[i] == null || !expectHeader.equals(headers[i].trim()))
-                            throw new ExperimentException("Expected '" + expectHeader + "' in column " + i + " of header line; found '" + headers[i].trim() + "' instead.");
-                    }
-
-                    columns[i] = cd;
-                }
-
-                String expectHeader = "% of Total Information";
-                if (groupHeaders[COL_VIABLE] == null || !expectHeader.equals(groupHeaders[COL_VIABLE].trim()))
-                    throw new ExperimentException("Expected '" + expectHeader + "' in column " + COL_VIABLE + " of group headers line");
+                ColumnDescriptor[] columns = null;
+                if (softwareName.equals("ViaCount"))
+                    columns = createViaCountColumns(groupHeaders, headers);
+                else if (softwareName.equals("ExpressPlus"))
+                    columns = createExpressPlusColumns(groupHeaders, headers);
 
                 TabLoader tl = new TabLoader(reader, false);
                 tl.setColumns(columns);
                 tl.setScanAheadLineCount(count);
                 tl.parseAsCSV();
-                _resultData = tl.load();
+
+                if (softwareName.equals("ExpressPlus"))
+                    _resultData = loadExpressPlus(tl.load());
+                else
+                    _resultData = tl.load();
             }
             finally
             {
                 if (reader != null) { try { reader.close(); } catch (IOException ioe) { } }
             }
         }
+
+        private ColumnDescriptor[] createColumns(
+                String[] groupHeaders, String[] headers,
+                final String sampleNumExpectedHeader,
+                final int groupCol, final String groupExpectedHeader,
+                final int viableCol, final String viableExpectedHeader,
+                final int totalViableCol, final String totalViableExpectedHeader,
+                final int totalCellsCol, final String totalCellsExpectedHeader,
+                final int originalVolumeCol, final String originalVolumeExpectedHeader)
+            throws ExperimentException
+        {
+            final int COL_SAMPLE_NUM = 0;
+            final int COL_SAMPLE_ID = 1;
+
+            String groupHeader = groupHeaders[groupCol];
+            if (groupHeader == null || !groupExpectedHeader.equals(groupHeader.trim()))
+                throw new ExperimentException("Expected '" + groupExpectedHeader + "' in column " + groupCol + " of group headers line; found '" + groupHeader.trim() + "' instead.");
+
+            ColumnDescriptor[] columns = new ColumnDescriptor[headers.length];
+            for (int i = 0; i < headers.length; i++)
+            {
+                ColumnDescriptor cd = new ColumnDescriptor();
+                String expectHeader = null;
+
+                if (i == COL_SAMPLE_NUM)
+                {
+                    cd.name = ViabilityAssayProvider.SAMPLE_NUM_PROPERTY_NAME;
+                    expectHeader = sampleNumExpectedHeader;
+                    cd.clazz = Integer.class;
+                }
+                else if (i == COL_SAMPLE_ID)
+                {
+                    cd.name = ViabilityAssayProvider.POOL_ID_PROPERTY_NAME;
+                    expectHeader = "Sample ID";
+                    cd.clazz = String.class;
+                }
+                else if (i == viableCol)
+                {
+                    cd.name = ViabilityAssayProvider.VIABILITY_PROPERTY_NAME;
+                    expectHeader = viableExpectedHeader;
+                    cd.clazz = Double.class;
+                    cd.converter = new Converter()
+                    {
+                        public Object convert(Class type, Object value)
+                        {
+                            Double d = (Double) ConvertUtils.convert((String)value, Double.class);
+                            if (d != null)
+                                return d.doubleValue() / 100;
+                            return null;
+                        }
+                    };
+                }
+                else if (i == totalViableCol)
+                {
+                    cd.name = ViabilityAssayProvider.VIABLE_CELLS_PROPERTY_NAME;
+                    expectHeader = totalViableExpectedHeader;
+                    cd.clazz = Double.class;
+                }
+                else if (i == totalCellsCol)
+                {
+                    cd.name = ViabilityAssayProvider.TOTAL_CELLS_PROPERTY_NAME;
+                    expectHeader = totalCellsExpectedHeader;
+                    cd.clazz = Double.class;
+                }
+                else if (i == originalVolumeCol)
+                {
+                    // NOTE: Currently not imported into the domain -- just used for calculations.
+                    cd.name = "OriginalVolume";
+                    expectHeader = originalVolumeExpectedHeader;
+                    cd.clazz = Double.class;
+                }
+                else
+                {
+                    cd.load = false;
+                }
+
+                if (expectHeader != null)
+                {
+                    assert cd.load;
+                    if (headers[i] == null || !expectHeader.equals(headers[i].trim()))
+                        throw new ExperimentException("Expected '" + expectHeader + "' in column " + i + " of header line; found '" + headers[i].trim() + "' instead.");
+                }
+
+                columns[i] = cd;
+            }
+
+            return columns;
+        }
+
+        private ColumnDescriptor[] createViaCountColumns(String[] groupHeaders, String[] headers)
+                throws ExperimentException
+        {
+            final int COL_GROUP = 11; // cell 'L'
+            final int COL_VIABLE = 11; // cell 'L'
+            final int COL_TOTAL_VIABLE = 42; // cell 'AQ'
+            final int COL_TOTAL_CELLS = 45; // cell 'AT'
+
+            return createColumns(groupHeaders, headers,
+                    "Sample No.",
+                    COL_GROUP, "% of Total Information",
+                    COL_VIABLE, "Viable",
+                    COL_TOTAL_VIABLE, "Total Viable Cells in Original Sample",
+                    COL_TOTAL_CELLS, "Total Cells in Original Sample",
+                    -1, null);
+        }
+
+        // NOTE: These columns are hard coded to the Letvin sample file.  There doesn't seem to be any
+        // indication in the file which group contains the actual viability information.
+        private ColumnDescriptor[] createExpressPlusColumns(String[] groupHeaders, String[] headers)
+                throws ExperimentException
+        {
+            final int COL_GROUP = 154; // cell 'EY'
+            final int COL_VIABLE = COL_GROUP+2; // cell 'FA'
+            final int COL_TOTAL_VIABLE = COL_GROUP+1; // cell 'EZ', need to multiply by original volume in cell 'G'
+            // not in the spreadsheet: total cells = total viable / %viable
+            //final int COL_TOTAL_CELLS = COL_GROUP+1;
+            final int COL_ORIGINAL_VOLUME = 6; // cell 'G'
+
+            return createColumns(groupHeaders, headers,
+                    "Sample Number",
+                    COL_GROUP, "Quad Stat - UpperLeft Region (Plot 3)",
+                    COL_VIABLE, "% of Total Cells",
+                    COL_TOTAL_VIABLE, "Cells/mL",
+                    -1, null,
+                    COL_ORIGINAL_VOLUME, "Original Volume");
+        }
+
+        // Adjust the total viable cells (based on volume) and calculate the total cells.
+        private List<Map<String, Object>> loadExpressPlus(List<Map<String, Object>> rows)
+        {
+            List<Map<String, Object>> newRows = new ArrayList<Map<String, Object>>();
+            for (Map<String, Object> row : rows)
+            {
+                Map<String, Object> newRow = new HashMap<String, Object>(row);
+                Double volume = (Double)row.get("OriginalVolume");
+                Double viableCells = volume * (Double)row.get(ViabilityAssayProvider.VIABLE_CELLS_PROPERTY_NAME);
+
+                Double viability = (Double)row.get(ViabilityAssayProvider.VIABILITY_PROPERTY_NAME);
+                Double totalCells = viableCells / viability;
+
+                newRow.put(ViabilityAssayProvider.VIABLE_CELLS_PROPERTY_NAME, viableCells);
+                newRow.put(ViabilityAssayProvider.TOTAL_CELLS_PROPERTY_NAME, totalCells);
+                newRows.add(newRow);
+            }
+
+            return newRows;
+        }
+
     }
 
     public void importTransformDataMap(ExpData data, User user, ExpRun run, ExpProtocol protocol, AssayProvider provider, List<Map<String, Object>> dataMap) throws ExperimentException
@@ -287,6 +394,34 @@ public class GuavaDataHandler extends ViabilityAssayDataHandler implements Trans
             assertTrue(Math.abs(0.954 - (Double)row.get("Viability")) < 0.0001);
             assertEquals(25878380.0, row.get(ViabilityAssayProvider.VIABLE_CELLS_PROPERTY_NAME));
             assertEquals(27126184.0, row.get(ViabilityAssayProvider.TOTAL_CELLS_PROPERTY_NAME));
+        }
+
+        @Test
+        public void testExpressPlus() throws Exception
+        {
+            AppProps props = AppProps.getInstance();
+            String projectRootPath =  props.getProjectRoot();
+            File projectRoot = new File(projectRootPath);
+
+            File viabilityFiles = new File(projectRoot, "sampledata/viability");
+            assertTrue("Expected to find viability test files: " + viabilityFiles.getAbsolutePath(), viabilityFiles.exists());
+
+            GuavaDataHandler.Parser parser = new GuavaDataHandler.Parser(null, null, new File(viabilityFiles, "122810.EP5.csv"));
+
+            List<Map<String, Object>> rows = parser.getResultData();
+            assertEquals("Expected 16 rows", 16, rows.size());
+
+            Map<String, Object> row = rows.get(0);
+            assertEquals(7, row.size());
+            assertEquals(1, row.get(ViabilityAssayProvider.SAMPLE_NUM_PROPERTY_NAME));
+            assertEquals("B01", row.get(ViabilityAssayProvider.POOL_ID_PROPERTY_NAME));
+            assertEquals("B01", row.get(ViabilityAssayProvider.PARTICIPANTID_PROPERTY_NAME));
+            assertFalse(row.containsKey(ViabilityAssayProvider.VISITID_PROPERTY_NAME));
+            assertTrue(Math.abs(0.4542 - (Double)row.get("Viability")) < 0.0001);
+            assertEquals(9043848.8, row.get(ViabilityAssayProvider.VIABLE_CELLS_PROPERTY_NAME));
+            assertEquals(1.9911600176133864E7, row.get(ViabilityAssayProvider.TOTAL_CELLS_PROPERTY_NAME));
+            assertEquals(10.0, row.get("OriginalVolume"));
+
         }
     }
 }
