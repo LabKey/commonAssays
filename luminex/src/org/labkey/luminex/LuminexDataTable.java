6931584309f4a486450264c89cd0ced2baa47506
Matthew Bellew <mbellew@labkey.com>
2011-05-26 22:40:19 +0000

    AbstractDataIterator swtich from ValidationException -> BatchValidationException

--- luminex/src/org/labkey/luminex/LuminexDataTable.java
+++ luminex/src/org/labkey/luminex/LuminexDataTable.java
@@ -32,10 +32,10 @@ import org.labkey.api.exp.property.Domain;
 import org.labkey.api.exp.property.DomainProperty;
 import org.labkey.api.exp.query.ExpDataTable;
 import org.labkey.api.exp.query.ExpSchema;
+import org.labkey.api.query.BatchValidationException;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.FilteredTable;
 import org.labkey.api.query.LookupForeignKey;
-import org.labkey.api.query.ValidationException;
 import org.labkey.api.security.User;
 import org.labkey.api.study.assay.AbstractAssayProvider;
 import org.labkey.api.study.assay.AssayService;
@@ -242,7 +242,7 @@ public class LuminexDataTable extends FilteredTable implements UpdateableTableIn
     }
 
     @Override
-    public int persistRows(DataIterator data, ValidationException errors)
+    public int persistRows(DataIterator data, BatchValidationException errors)
     {
         Table.TableLoaderPump pump = new Table.TableLoaderPump(data, this, errors);
         pump.run();
