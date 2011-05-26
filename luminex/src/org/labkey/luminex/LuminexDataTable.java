f763952c91340a8c5eb212dca08d64223c52737a
Matthew Bellew <mbellew@labkey.com>
2011-05-26 20:34:23 +0000

    class StandardETL TableInfo.persistRows()

--- luminex/src/org/labkey/luminex/LuminexDataTable.java
+++ luminex/src/org/labkey/luminex/LuminexDataTable.java
@@ -26,6 +26,7 @@ import org.labkey.api.data.SQLFragment;
 import org.labkey.api.data.Table;
 import org.labkey.api.data.TableInfo;
 import org.labkey.api.data.UpdateableTableInfo;
+import org.labkey.api.etl.DataIterator;
 import org.labkey.api.exp.api.ExpProtocol;
 import org.labkey.api.exp.property.Domain;
 import org.labkey.api.exp.property.DomainProperty;
@@ -34,6 +35,7 @@ import org.labkey.api.exp.query.ExpSchema;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.FilteredTable;
 import org.labkey.api.query.LookupForeignKey;
+import org.labkey.api.query.ValidationException;
 import org.labkey.api.security.User;
 import org.labkey.api.study.assay.AbstractAssayProvider;
 import org.labkey.api.study.assay.AssayService;
@@ -237,6 +239,14 @@ public class LuminexDataTable extends FilteredTable implements UpdateableTableIn
     public CaseInsensitiveHashSet skipProperties()
     {
         return new CaseInsensitiveHashSet();
+    }
+
+    @Override
+    public int persistRows(DataIterator data, ValidationException errors)
+    {
+        Table.TableLoaderPump pump = new Table.TableLoaderPump(data, this, errors);
+        pump.run();
+        return pump.getRowCount();
     }
 
     @Override
