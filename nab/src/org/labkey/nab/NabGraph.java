97c055caefbc1c7cc75f397f9dad6302f603d5d0
Adam Rauch <adam@labkey.com>
2011-12-23 13:48:36 +0000

    Update licenses

--- nab/src/org/labkey/nab/NabGraph.java
+++ nab/src/org/labkey/nab/NabGraph.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2010-2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.labkey.nab;
 
 import org.jfree.chart.ChartColor;
@@ -24,21 +39,6 @@ import java.io.IOException;
 import java.util.*;
 
 /**
- * Copyright (c) 2010-2011 LabKey Corporation
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p/>
- * <p/>
  * User: brittp
  * Date: Apr 21, 2010 12:50:55 PM
  */
