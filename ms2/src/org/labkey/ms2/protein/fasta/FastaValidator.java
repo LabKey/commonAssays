446d100f28545c27ccc778cdb28f96838bab9791
Josh Eckels <jeckels@labkey.com>
2012-01-10 02:13:15 +0000

    Issue 13768: **OutOfMemoryError parsing very large FASTA file

--- ms2/src/org/labkey/ms2/protein/fasta/FastaValidator.java
+++ ms2/src/org/labkey/ms2/protein/fasta/FastaValidator.java
@@ -15,14 +15,19 @@
  */
 package org.labkey.ms2.protein.fasta;
 
-import org.labkey.api.collections.Sets;
+import org.ardverk.collection.ByteArrayKeyAnalyzer;
+import org.ardverk.collection.PatriciaTrie;
+import org.ardverk.collection.StringKeyAnalyzer;
+import org.labkey.api.util.UnexpectedException;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.text.Format;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 /**
  * User: adam
@@ -42,34 +47,42 @@ public class FastaValidator
 
     public List<String> validate()
     {
-        Set<String> proteinNames = Sets.newCaseInsensitiveHashSet(1000);
+        // Use a trie to get space-efficient storage
+        Map<byte[], Object> proteinNames = new PatriciaTrie<byte[], Object>(ByteArrayKeyAnalyzer.VARIABLE);
         List<String> errors = new ArrayList<String>();
         Format lineFormat = DecimalFormat.getIntegerInstance();
         ProteinFastaLoader curLoader = new ProteinFastaLoader(_fastaFile);
 
-        //noinspection ForLoopReplaceableByForEach
-        for (ProteinFastaLoader.ProteinIterator proteinIterator = curLoader.iterator(); proteinIterator.hasNext();)
+        try
         {
-            Protein protein = proteinIterator.next();
-
-            // Creating a new string here is critical for handling large FASTA files -- see issue #6441 
-            //noinspection RedundantStringConstructorCall
-            String lookup = new String(protein.getLookup());
-
-            if (proteinNames.contains(lookup))
+            //noinspection ForLoopReplaceableByForEach
+            for (ProteinFastaLoader.ProteinIterator proteinIterator = curLoader.iterator(); proteinIterator.hasNext();)
             {
-                errors.add("Line " + lineFormat.format(proteinIterator.getLastHeaderLineNum()) + ": " + lookup + " is a duplicate protein name");
+                Protein protein = (Protein)proteinIterator.next();
 
-                if (errors.size() > 999)
+                // Use UTF-8 encoding so that we only use a single byte for ASCII characters
+                String lookupString = protein.getLookup().toLowerCase();
+                byte[] lookup = lookupString.getBytes("UTF-8");
+
+                if (proteinNames.containsKey(lookup))
                 {
-                    errors.add("Stopped validating after 1,000 errors");
-                    break;
+                    errors.add("Line " + lineFormat.format(proteinIterator.getLastHeaderLineNum()) + ": " + lookupString + " is a duplicate protein name");
+
+                    if (errors.size() > 999)
+                    {
+                        errors.add("Stopped validating after 1,000 errors");
+                        break;
+                    }
+                }
+                else
+                {
+                    proteinNames.put(lookup, null);
                 }
             }
-            else
-            {
-                proteinNames.add(lookup);
-            }
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            throw new UnexpectedException(e);
         }
 
         return errors;
