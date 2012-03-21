74db10da77e975717a854c06684acd7a76a95724
Adam Rauch <adam@labkey.com>
2012-03-21 14:54:20 +0000

    Issue 13503: **NullPointerException navigating to specimens tab via bookmark or emailed link for deleted study Issue 14383: **slow performance for autocomplete in manage groups page Issue 14312: **Need small icons for attachment reports Issue 14275: **Long labels act strangely when "mean" is selected
    NavTree no longer extends Pair.
    
    Remove some mangled licenses.
    
    Allow selenium tests on Firefox 11.

--- nab/src/org/labkey/nab/GetNabRunsBaseForm.java
+++ nab/src/org/labkey/nab/GetNabRunsBaseForm.java
@@ -4,23 +4,9 @@ import org.labkey.api.action.HasViewContext;
 import org.labkey.api.view.ViewContext;
 
 /**
-* Copyright (c) 2010 LabKey Corporation
-* <p/>
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* <p/>
-* http://www.apache.org/licenses/LICENSE-2.0
-* <p/>
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* <p/>
-* User: brittp
-* Date: Mar 12, 2010 9:47:58 AM
-*/
+ * User: brittp
+ * Date: Mar 12, 2010 9:47:58 AM
+ */
 public abstract class GetNabRunsBaseForm implements HasViewContext
 {
     private ViewContext _viewContext;
--- nab/src/org/labkey/nab/NabRunPropertyMap.java
+++ nab/src/org/labkey/nab/NabRunPropertyMap.java
@@ -13,23 +13,9 @@ import java.util.List;
 import java.util.Map;
 
 /**
-* Copyright (c) 2010-2011 LabKey Corporation
-* <p/>
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* <p/>
-* http://www.apache.org/licenses/LICENSE-2.0
-* <p/>
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* <p/>
-* User: brittp
-* Date: Mar 12, 2010 9:43:44 AM
-*/
+ * User: brittp
+ * Date: Mar 12, 2010 9:43:44 AM
+ */
 public class NabRunPropertyMap extends HashMap<String, Object>
 {
     private static class PropertyNameMap extends HashMap<String, Object>
