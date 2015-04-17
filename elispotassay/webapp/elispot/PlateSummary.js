f2e974318b15396398183b2b8497e6d340c750e9
Nick Kerr <nicka@labkey.com>
2015-04-17 17:38:23 +0000

    Client API: requiresExt3, requiresExt4Sandbox, requiresExt3ClientAPI, requiresExt4ClientAPI no longer need 'immediate' parameter. Compatibility maintained for old function signatures.

--- elispotassay/webapp/elispot/PlateSummary.js
+++ elispotassay/webapp/elispot/PlateSummary.js
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
-LABKEY.requiresExt4Sandbox(true);
+LABKEY.requiresExt4Sandbox();
 
 Ext4.define('LABKEY.ext4.PlateSummary', {
 
