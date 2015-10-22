0b90ca024960da9ce144ca01fbe51fa938c54941
Karl Lum <klum@labkey.com>
2015-10-22 19:23:10 +0000

    move the NAb dilution upgrade code to a pipeline job

--- nab/src/org/labkey/nab/NabUpgradeCode.java
+++ nab/src/org/labkey/nab/NabUpgradeCode.java
@@ -30,11 +30,22 @@ import org.labkey.api.exp.ExperimentException;
 import org.labkey.api.exp.api.ExpProtocol;
 import org.labkey.api.exp.api.ExpRun;
 import org.labkey.api.module.ModuleContext;
+import org.labkey.api.pipeline.PipeRoot;
+import org.labkey.api.pipeline.PipelineJob;
+import org.labkey.api.pipeline.PipelineService;
+import org.labkey.api.pipeline.PipelineUrls;
+import org.labkey.api.pipeline.PipelineValidationException;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.study.assay.AssayProvider;
 import org.labkey.api.study.assay.AssayService;
+import org.labkey.api.util.DateUtil;
+import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.util.Pair;
+import org.labkey.api.util.URLHelper;
+import org.labkey.api.view.ViewBackgroundInfo;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -56,6 +67,47 @@ public class NabUpgradeCode implements UpgradeCode
     {
         if (!context.isNewInstall())
         {
+            try
+            {
+                Container c = ContainerManager.getSharedContainer();
+                ViewBackgroundInfo info = new ViewBackgroundInfo(c, context.getUpgradeUser(), PageFlowUtil.urlProvider(PipelineUrls.class).urlBegin(c));
+                NabDilutionUpgradeJob job = new NabDilutionUpgradeJob("NAb Upgrade Provider", info, PipelineService.get().findPipelineRoot(c));
+
+                PipelineService.get().queueJob(job);
+            }
+            catch (Exception e)
+            {
+                _log.error("NAb dilution table upgrade failed", e);
+            }
+        }
+    }
+
+    private static class NabDilutionUpgradeJob extends PipelineJob
+    {
+        public static final String PROCESSING_STATUS = "Processing";
+
+        public NabDilutionUpgradeJob(String provider, ViewBackgroundInfo info, PipeRoot root) throws IOException, SQLException
+        {
+            super(provider, info, root);
+
+            File logFile = File.createTempFile("nabDilutionUpgrade", ".log", root.getRootPath());
+            setLogFile(logFile);
+        }
+
+        @Override
+        public URLHelper getStatusHref()
+        {
+            return null;
+        }
+
+        @Override
+        public String getDescription()
+        {
+            return "NAb upgrade dilution data to new tables.";
+        }
+
+        public void run()
+        {
             int runCount = 0;
             int protocolCount = 0;
             Set<ExpProtocol> protocols = new HashSet<>();   // protocols may be accessible by more than one container
@@ -66,6 +118,9 @@ public class NabUpgradeCode implements UpgradeCode
                     for (ExpProtocol protocol : AssayService.get().getAssayProtocols(container))
                         protocols.add(protocol);
             }
+
+            setStatus(PROCESSING_STATUS, "Job started at: " + DateUtil.nowISO());
+            info("Starting NAb dilution table upgrade for " + protocols.size() + " assay instances.");
 
             for (ExpProtocol protocol : protocols)
             {
@@ -90,40 +145,41 @@ public class NabUpgradeCode implements UpgradeCode
                         {
                             if (wellGroupNameToNabSpecimen.isEmpty())
                             {
-                                _log.warn(dilutionDataHandler.getResourceName(run) + " run data could not be found for run " + run.getRowId() +  " (" +
+                                warn(dilutionDataHandler.getResourceName(run) + " run data could not be found for run " + run.getRowId() + " (" +
                                         run.getName() + ") in container '" + run.getContainer().getPath() +
                                         "'. Run details will not be available. Continuing upgrade for other runs.");
                             }
                             else
                             {
-                                dilutionDataHandler.populateWellData(protocol, run, context.getUpgradeUser(), cutoffFormats, wellGroupNameToNabSpecimen);
+                                dilutionDataHandler.populateWellData(protocol, run, getUser(), cutoffFormats, wellGroupNameToNabSpecimen);
                             }
                         }
                         catch (DilutionDataHandler.MissingDataFileException e)
                         {
-                            _log.warn(dilutionDataHandler.getResourceName(run) + " data file could not be found for run " + run.getRowId() + " (" +
+                            warn(dilutionDataHandler.getResourceName(run) + " data file could not be found for run " + run.getRowId() + " (" +
                                     run.getName() + ") in container '" + run.getContainer().getPath() +
                                     "'. Deleted from file system? Run details will not be available. Continuing upgrade for other runs.");
                         }
                         catch (ExperimentException | SQLException e)
                         {
-                            _log.warn("Run " + run.getRowId() + " (" + run.getName() + ") in container '" +
+                            warn("Run " + run.getRowId() + " (" + run.getName() + ") in container '" +
                                     run.getContainer().getPath() + "' failed to upgrade due to exception: " +
                                     e.getMessage() + ". Continuing upgrade for other runs.");
                             for (StackTraceElement stackTraceElement : e.getStackTrace())
                             {
-                                _log.info("\t\t" + stackTraceElement.toString());
+                               warn("\t\t" + stackTraceElement.toString());
                             }
-                            _log.info("");
+                            warn("");
                         }
 
                         if ((runCount % 500) == 0)
-                            _log.info("Runs processed: " + runCount);
+                            info("Runs processed: " + runCount);
 
                     }
                 }
             }
-            _log.info("Total runs processed: " + runCount + "; Total protocols: " + protocolCount);
+            info("Total runs processed: " + runCount + "; Total protocols: " + protocolCount);
+            setStatus(TaskStatus.complete, "Job finished at: " + DateUtil.nowISO());
         }
     }
 }
