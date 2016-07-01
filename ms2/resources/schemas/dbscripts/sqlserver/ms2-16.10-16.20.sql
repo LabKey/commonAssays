3d0808b52662d64d60c28208262b2721634d05ab
Adam Rauch <adam@labkey.com>
2016-07-01 14:47:00 +0000

    Consolidate scripts to 16.10-16.20

---
+++ ms2/resources/schemas/dbscripts/sqlserver/ms2-16.10-16.20.sql
@@ -0,0 +1,4 @@
+/* ms2-16.10-16.11.sql */
+
+-- Issue 26074: Equals filter fails on protein.Sequence.ProtSequence column on SqlServer
+ALTER TABLE prot.Sequences ALTER COLUMN ProtSequence VARCHAR(MAX);
