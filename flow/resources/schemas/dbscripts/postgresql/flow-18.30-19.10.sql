868b9e6de3ce636f7dfb4367f97c4fc20afbfd71
Dave Bradlee <dave@labkey.com>
2019-02-26 00:55:40 +0000

    Script consolidation for 19.1 (except CDS)

---
+++ flow/resources/schemas/dbscripts/postgresql/flow-18.30-19.10.sql
@@ -0,0 +1,1 @@
+UPDATE flow.object SET uri = 'file:///' || substr(uri, 7) WHERE uri LIKE 'file:/_%' AND uri NOT LIKE 'file:///%' AND uri IS NOT NULL;
---
+++ flow/resources/schemas/dbscripts/sqlserver/flow-18.30-19.10.sql
@@ -0,0 +1,1 @@
+UPDATE flow.object SET uri = 'file:///' + substring(uri, 7, 400) WHERE uri LIKE 'file:/_%' AND uri NOT LIKE 'file:///%' AND uri IS NOT NULL;
