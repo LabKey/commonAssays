1f89e42434424f7f188138bc230f7e789e6505e7
Adam Rauch <adam@labkey.com>
2020-02-04 15:41:11 -0800

    Remove ProteinManager.indexProteins() (#121)

--- ms2/src/org/labkey/ms2/protein/FastaDbLoader.java
+++ ms2/src/org/labkey/ms2/protein/FastaDbLoader.java
@@ -180,8 +180,6 @@ public class FastaDbLoader extends DefaultAnnotationLoader
                 sharedIdentsStrategy.close();
             }
         }
-
-        ProteinManager.indexProteins(null, (Date)null);
     }
 
     public int getFastaId()
