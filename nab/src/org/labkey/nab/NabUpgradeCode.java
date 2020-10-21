2adb709a072b2cd453a81ac2458e0d0c3eafee0b
cnathe <cnathe@labkey.com>
2020-10-21 11:08:46 -0500

    Create and store the pipeline job, but let the JobRestarter queue and run it

--- nab/src/org/labkey/nab/NabUpgradeCode.java
+++ nab/src/org/labkey/nab/NabUpgradeCode.java
@@ -7,6 +7,8 @@ import org.labkey.api.data.DeferredUpgrade;
 import org.labkey.api.data.UpgradeCode;
 import org.labkey.api.module.ModuleContext;
 import org.labkey.api.pipeline.PipeRoot;
+import org.labkey.api.pipeline.PipelineJob;
+import org.labkey.api.pipeline.PipelineJobService;
 import org.labkey.api.pipeline.PipelineService;
 import org.labkey.api.pipeline.PipelineValidationException;
 import org.labkey.api.view.ViewBackgroundInfo;
@@ -27,9 +29,15 @@ public class NabUpgradeCode implements UpgradeCode
             {
                 ViewBackgroundInfo info = new ViewBackgroundInfo(ContainerManager.getRoot(), null, null);
                 PipeRoot root = PipelineService.get().findPipelineRoot(ContainerManager.getRoot());
-                PipelineService.get().queueJob(new NabPopulateFitParametersPipelineJob(info, root));
+
+                // Create a new pipeline job and add it to the JobStore. Since this code runs before the module
+                // startBackgroundThreads(), the PipelineModule.startBackgroundThreads JobRestarter will pick up this
+                // new job and queue/run it.
+                NabPopulateFitParametersPipelineJob job = new NabPopulateFitParametersPipelineJob(info, root);
+                PipelineJobService.get().getStatusWriter().setStatus(job, PipelineJob.TaskStatus.waiting.toString(), null, true);
+                PipelineJobService.get().getJobStore().storeJob(job);
             }
-            catch (PipelineValidationException e)
+            catch (Exception e)
             {
                 _log.error("Unexpected error during NabPopulateFitParametersPipelineJob", e);
             }
