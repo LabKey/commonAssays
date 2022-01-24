afb1a40364f15bea1a67ea0e845a7e0297c446b3
labkey-matthewb <matthewb@labkey.com>
2022-01-24 14:55:09 -0800

    Formats are not thread safe.  Get rid of static. (#422)

--- flow/enginesrc/org/labkey/flow/analysis/chart/FlowLinearAxis.java
+++ flow/enginesrc/org/labkey/flow/analysis/chart/FlowLinearAxis.java
@@ -10,16 +10,12 @@ import java.util.Locale;
  */
 public class FlowLinearAxis extends NumberAxis
 {
-    private static final NumberFormat TICK_FORMAT;
-
-    static {
-        TICK_FORMAT = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
-        TICK_FORMAT.setMaximumFractionDigits(2);
-    }
-
     public FlowLinearAxis(String label)
     {
         super(label);
-        setNumberFormatOverride(TICK_FORMAT);
+
+        var format = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
+        format.setMaximumFractionDigits(2);
+        setNumberFormatOverride(format);
     }
 }
