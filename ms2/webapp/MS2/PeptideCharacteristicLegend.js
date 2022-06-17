01cd084bf099c24f21c79cbbe4ca418244c832e4
ankurjuneja <ankurjuneja05@gmail.com>
2022-06-17 16:45:18 -0700

    display min, max and every 5th intensity with 2 decimal precision

--- ms2/webapp/MS2/PeptideCharacteristicLegend.js
+++ ms2/webapp/MS2/PeptideCharacteristicLegend.js
@@ -7,13 +7,17 @@ if (!LABKEY.ms2.PeptideCharacteristicLegend) {
 
         addHeatMap: function (peptideCharacteristics, colors) {
 
-            const y_axis = peptideCharacteristics.length * 10;
+            const len = peptideCharacteristics.length;
+            const min = peptideCharacteristics[0];
+            const max = peptideCharacteristics[len-1];
+            const y_axis = len * 10;
             const x_axis = 0;
 
             var rectWidth = 30;
+            var count = 0;
 
             var svgContainer = d3.select(".heatmap").append("svg")
-                    .attr("height", rectWidth * peptideCharacteristics.length + y_axis)
+                    .attr("height", rectWidth * len + y_axis)
                     .attr("width", 200);
 
             var rect = svgContainer.selectAll(".rect")
@@ -28,7 +32,19 @@ if (!LABKEY.ms2.PeptideCharacteristicLegend) {
             svgContainer.selectAll('.text')
                     .data(peptideCharacteristics)
                     .enter().append('text')
-                    .text((d) => d.toString())
+                    .text((d) => {
+                        var str = "";
+                        if (d.toString() === min.toString() || d.toString() === max.toString() || count === 4) {
+                           str = d.toPrecision(3).toString();
+                        }
+                        if (count === 4) {
+                            count = 0;
+                        }
+                        else {
+                            count++;
+                        }
+                        return str;
+                    })
                     .attr("y", (d,i) => y_axis + (rectWidth*i) + 25)
                     .attr("x", x_axis + 35);
 
