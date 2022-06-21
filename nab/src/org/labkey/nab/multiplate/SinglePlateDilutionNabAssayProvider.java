cc3529134032001de1171b718b30888eba951e74
labkey-jeckels <jeckels@labkey.com>
2022-06-21 14:23:41 -0700

    Issue 45272: Update AssayProvider.getDescription() to not return HTML in a java.lang.String

--- nab/src/org/labkey/nab/multiplate/SinglePlateDilutionNabAssayProvider.java
+++ nab/src/org/labkey/nab/multiplate/SinglePlateDilutionNabAssayProvider.java
@@ -77,10 +77,6 @@ public class SinglePlateDilutionNabAssayProvider extends HighThroughputNabAssayP
                 "assay differs from the standard NAb assay in that samples are identical across plates but with a different virus per plate. " +
                 "Dilutions are assumed to occur within a single plate.  Both NAb assay types measure neutralization in TZM-bl cells as a function of a " +
                 "reduction in Tat-induced luciferase (Luc) reporter gene expression after a single round of infection. Montefiori, D.C. 2004";
-/*                PageFlowUtil.popupHelp(HtmlString.unsafe("<a href=\"http://www.ncbi.nlm.nih.gov/pubmed/18432938\">" +
-                        "Evaluating neutralizing antibodies against HIV, SIV and SHIV in luciferase " +
-                        "reporter gene assays</a>.  Current Protocols in Immunology, (Coligan, J.E., " +
-                        "A.M. Kruisbeek, D.H. Margulies, E.M. Shevach, W. Strober, and R. Coico, eds.), John Wiley & Sons, 12.11.1-12.11.15."), "Nab").inlineScript().toString(); */
     }
 
     @Override
