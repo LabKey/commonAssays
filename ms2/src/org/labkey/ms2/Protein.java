070c015eb697fca519227898cca259f25230fd94
ankurjuneja <ankurjuneja05@gmail.com>
2022-09-23 12:06:46 -0700

    use linked hashmap for ordering of peptide sequences

--- ms2/src/org/labkey/ms2/Protein.java
+++ ms2/src/org/labkey/ms2/Protein.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -814,7 +815,7 @@ public class Protein
         List<Range> uncoalescedPeptideRanges = new ArrayList<>();
         if (_modifiedPeptideCharacteristics != null && !_modifiedPeptideCharacteristics.isEmpty())
         {
-            _modifiedPeptides = new HashMap<>();
+            _modifiedPeptides = new LinkedHashMap<>();
             // build the modifiedPeptideMap
             for (PeptideCharacteristic pep : _modifiedPeptideCharacteristics)
             {
