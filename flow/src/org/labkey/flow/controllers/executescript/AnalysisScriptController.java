5e89269e256685cbf366165734fcd6a534827410
labkey-tchad <tchad@labkey.com>
2022-11-08 16:38:33 -0800

    Check for another null

--- flow/src/org/labkey/flow/controllers/executescript/AnalysisScriptController.java
+++ flow/src/org/labkey/flow/controllers/executescript/AnalysisScriptController.java
@@ -45,6 +45,8 @@ import org.labkey.api.util.Pair;
 import org.labkey.api.util.URIUtil;
 import org.labkey.api.util.URLHelper;
 import org.labkey.api.view.ActionURL;
+import org.labkey.api.view.BadRequestException;
+import org.labkey.api.view.BadRequestException.HowBad;
 import org.labkey.api.view.HttpPostRedirectView;
 import org.labkey.api.view.HttpView;
 import org.labkey.api.view.JspView;
@@ -155,9 +157,9 @@ public class AnalysisScriptController extends BaseFlowController
         protected ModelAndView chooseAnalysisName(ChooseRunsToAnalyzeForm form, BindException errors)
         {
             nav = new Pair<>("Choose new analysis name", Action.chooseAnalysisName);
-            if (form.getProtocol() == null)
+            if (form.getProtocol() == null || form.getProtocolStep() == null)
             {
-                throw new NotFoundException("Invalid protocol specified");
+                throw new BadRequestException("Invalid wizard state");
             }
             return new JspView<>("/org/labkey/flow/controllers/executescript/chooseAnalysisName.jsp", form, errors);
         }
