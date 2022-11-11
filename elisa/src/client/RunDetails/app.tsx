bf3724274b4e3b9d71e0fa3b3e45898b5bd7c260
Cory Nathe <cnathe@labkey.com>
2022-11-11 11:35:46 -0600

    Package updates (#536)
    * Update @labkey/components and @labkey/build package versions to latest
    - fix for jest config with revert of ESModules build
    - package version updates for jest and related
    - move scss import to app/dev

--- elisa/src/client/RunDetails/app.tsx
+++ elisa/src/client/RunDetails/app.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from '@labkey/api';
 
 import { AppContext, RunDetails } from './RunDetails';
-import { App } from '@labkey/api';
+
+import './RunDetails.scss';
 
 App.registerApp<AppContext>('elisaRunDetails', (target: string, ctx: AppContext) => {
     ReactDOM.render(<RunDetails context={ctx} />, document.getElementById(target));
