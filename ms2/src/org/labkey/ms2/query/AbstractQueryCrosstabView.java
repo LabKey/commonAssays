9671c9d8e86e1d803c8e4f1b6e57e7086a38794e
labkey-jeckels <jeckels@labkey.com>
2023-11-30 17:31:57 -0800

    Remove GWT-based comparison overview venn diagram UI

--- ms2/src/org/labkey/ms2/query/AbstractQueryCrosstabView.java
+++ ms2/src/org/labkey/ms2/query/AbstractQueryCrosstabView.java
@@ -15,27 +15,12 @@
  */
 package org.labkey.ms2.query;
 
-import org.labkey.api.data.CrosstabTableInfo;
 import org.labkey.api.data.Sort;
-import org.labkey.api.exp.api.ExpExperiment;
-import org.labkey.api.exp.api.ExpRun;
-import org.labkey.api.exp.api.ExperimentService;
-import org.labkey.api.exp.api.ExperimentUrls;
-import org.labkey.api.gwt.client.model.GWTComparisonGroup;
-import org.labkey.api.gwt.client.model.GWTComparisonMember;
-import org.labkey.api.gwt.client.model.GWTComparisonResult;
 import org.labkey.api.query.QuerySettings;
 import org.labkey.api.query.QueryView;
-import org.labkey.api.util.PageFlowUtil;
-import org.labkey.api.view.ActionURL;
 import org.labkey.api.view.DataView;
 import org.labkey.api.view.ViewContext;
 import org.labkey.ms2.MS2Controller;
-import org.labkey.ms2.MS2Run;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * User: jeckels
@@ -72,44 +57,5 @@ public abstract class AbstractQueryCrosstabView extends ComparisonCrosstabView
         view.getRenderContext().setViewContext(getViewContext());
         view.getRenderContext().setBaseSort(getBaseSort());
         return view;
-    }
-
-    @Override
-    protected GWTComparisonResult createComparisonResult(boolean[][] hits, CrosstabTableInfo table)
-    {
-        List<MS2Run> runs = _schema.getRuns();
-        GWTComparisonMember[] gwtMembers = new GWTComparisonMember[runs.size()];
-        Map<Integer, GWTComparisonGroup> groups = new HashMap<>();
-        for (int i = 0; i < runs.size(); i++)
-        {
-            MS2Run run = runs.get(i);
-            String lsid = run.getExperimentRunLSID();
-            ExpRun expRun = null;
-            if (lsid != null)
-            {
-                expRun = ExperimentService.get().getExpRun(lsid);
-            }
-
-            GWTComparisonMember gwtMember = new GWTComparisonMember(run.getDescription(), hits[i]);
-            ActionURL runURL = MS2Controller.MS2UrlsImpl.get().getShowRunUrl(getUser(), run);
-            gwtMember.setUrl(runURL.toString());
-            gwtMembers[i] = gwtMember;
-            if (expRun != null)
-            {
-                for (ExpExperiment experiment : expRun.getExperiments())
-                {
-                    GWTComparisonGroup group = groups.get(experiment.getRowId());
-                    if (group == null)
-                    {
-                        group = new GWTComparisonGroup();
-                        group.setURL(PageFlowUtil.urlProvider(ExperimentUrls.class).getExperimentDetailsURL(experiment.getContainer(), experiment).toString());
-                        group.setName(experiment.getName());
-                        groups.put(experiment.getRowId(), group);
-                    }
-                    group.addMember(gwtMember);
-                }
-            }
-        }
-        return new GWTComparisonResult(gwtMembers, groups.values().toArray(new GWTComparisonGroup[groups.size()]), hits[0].length, "Runs");
     }
 }
