5acf5678ba418c876bf0d504b2c1300766ff2a22
Adam Rauch <adam@labkey.com>
2025-03-13 17:18:13 -0700

    CR CR

--- flow/src/org/labkey/flow/controllers/executescript/SamplesConfirmGridView.java
+++ flow/src/org/labkey/flow/controllers/executescript/SamplesConfirmGridView.java
@@ -46,7 +46,7 @@ import org.labkey.api.util.HtmlString;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.util.SimpleNamedObject;
 import org.labkey.api.util.StringExpression;
-import org.labkey.api.util.element.Input;
+import org.labkey.api.util.element.Input.InputBuilder;
 import org.labkey.api.view.GridView;
 import org.labkey.api.writer.HtmlWriter;
 import org.labkey.flow.analysis.model.ISampleInfo;
@@ -335,7 +335,7 @@ public class SamplesConfirmGridView extends GridView
         {
             // Add a hidden input for spring form binding -- if this value is posted, the row was unchecked.
             out.write(
-                new Input.InputBuilder<>()
+                new InputBuilder<>()
                     .type("hidden")
                     .name(SpringActionController.FIELD_MARKER + getRecordSelectorName(ctx))
                     .value(0)
@@ -515,7 +515,8 @@ public class SamplesConfirmGridView extends GridView
                 String sampleId = ctx.get(SAMPLE_ID_FIELD_KEY, String.class);
                 for (FlowFCSFile candidate : candidates)
                 {
-                    out.write(new Input.InputBuilder<>().type("hidden").name("selectedSamples.rows[" + sampleId + "].candidateFile").value(candidate.getRowId()));
+                    out.write(new InputBuilder<>().type("hidden").name("selectedSamples.rows[" + sampleId + "].candidateFile").value(candidate.getRowId()));
+                    out.write("\n");
                 }
             }
         }
