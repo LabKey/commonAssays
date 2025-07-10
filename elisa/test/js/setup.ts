5a33f753df5489084ed36a89d3ceb6f3eeafe295
labkey-nicka <nickk@labkey.com>
2025-07-10 13:00:15 -0700

    jest.config.js

--- elisa/test/js/setup.ts
+++ elisa/test/js/setup.ts
@@ -1,1 +1,5 @@
 import "@testing-library/jest-dom"; // add custom jest matchers from jest-dom
+import { App } from "@labkey/components";
+
+// Configure @labkey/components to recognize this as a testing environment
+App.setIsTestEnv(true);
