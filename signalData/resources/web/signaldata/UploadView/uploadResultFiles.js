620cd31203bb8844669be7fa6e5ea943829d7f9a
Lum <klum@labkey.com>
2025-08-26 12:22:51 -0700

    revert prior change

--- signalData/resources/web/signaldata/UploadView/uploadResultFiles.js
+++ signalData/resources/web/signaldata/UploadView/uploadResultFiles.js
@@ -40,7 +40,7 @@ LABKEY.SignalData.initializeDataFileUploadForm = function (metadataFormId, eleme
         workingDirectory: getTempFolderName(),
         flex: 2,
         results: resultFileContents,
-        resultFields: assay.domains[assay.name + ' Results Fields'],
+        resultFields: assay.domains[assay.name + ' Result Fields'],
         listeners: {
             removefile: function(name, count) {
                 if (count == 0) {
