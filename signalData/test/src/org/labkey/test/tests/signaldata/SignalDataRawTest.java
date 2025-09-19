bca56c1f6d066f176dd6e1be11f03996a75eced3
labkey-danield <danield@labkey.com>
2025-09-19 13:49:49 -0700

    Add error tests for Signal Data tests. Cover Issue 53786

--- signalData/test/src/org/labkey/test/tests/signaldata/SignalDataRawTest.java
+++ signalData/test/src/org/labkey/test/tests/signaldata/SignalDataRawTest.java
@@ -23,16 +23,22 @@ import org.junit.experimental.categories.Category;
 import org.labkey.test.BaseWebDriverTest;
 import org.labkey.test.Locator;
 import org.labkey.test.categories.Daily;
+import org.labkey.test.pages.ReactAssayDesignerPage;
 import org.labkey.test.pages.signaldata.SignalDataAssayBeginPage;
 import org.labkey.test.pages.signaldata.SignalDataRunViewerPage;
 import org.labkey.test.pages.signaldata.SignalDataUploadPage;
+import org.labkey.test.params.FieldDefinition;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.Ext4Helper;
+import org.labkey.test.components.ext4.Window;
 import org.labkey.test.util.PostgresOnlyTest;
+import org.labkey.test.util.data.TestDataUtils;
 import org.labkey.test.util.signaldata.SignalDataInitializer;
 import org.openqa.selenium.WebElement;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -84,14 +90,14 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
     @Before
     public void preTest()
     {
-        // Reset to the original run/data file set created in the initialize
-        navigateToAssayLandingPage().resetUploadedData(DEFAULT_RUN);
+        // Reset to the original run/data file set created in the initializing
+        navigateToAssayLandingPage(SignalDataInitializer.RAW_SignalData_ASSAY).resetUploadedData(DEFAULT_RUN);
     }
 
     @Test
     public void testRunsSearch()
     {
-        SignalDataAssayBeginPage beginPage = navigateToAssayLandingPage();
+        SignalDataAssayBeginPage beginPage = navigateToAssayLandingPage(SignalDataInitializer.RAW_SignalData_ASSAY);
 
         //Test search by file
         beginPage.setSearchBox(RESULT_FILENAME_1);
@@ -107,7 +113,7 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
     public void testRunViewer()
     {
         // TODO: Test the run viewer. See FormulationsTest.qualityControlHPLCData for guidance.
-        SignalDataAssayBeginPage beginPage = navigateToAssayLandingPage();
+        SignalDataAssayBeginPage beginPage = navigateToAssayLandingPage(SignalDataInitializer.RAW_SignalData_ASSAY);
 
         beginPage.selectData(RESULT_FILENAME_1, DEFAULT_RUN);
         beginPage.selectData(RESULT_FILENAME_2, DEFAULT_RUN);
@@ -132,7 +138,8 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
         File metadataFile = getFile("RunsMetadata/datafiles.tsv");
         Map<String, List<String>> expectedData = Map.of("StringValue", List.of("StringOne", "StringTwo", "StringThree"),
                 "IntegerValue", List.of("1", "2", "3"));
-        SignalDataAssayBeginPage beginPage = importRun("importTest1",
+        SignalDataAssayBeginPage beginPage = importRun(SignalDataInitializer.RAW_SignalData_ASSAY,
+                "importTest1",
                 metadataFile,
                 List.of(getFile(String.join("/", ASSAY_DATA_LOC, "BLANK235.TXT"))),
                 List.of(
@@ -150,10 +157,11 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
         uploadPage.setRunIDField("cleared run");
         assertElementPresent(Ext4Helper.Locators.getGridRow()); //Check grid has elements
         uploadPage.clearRun();
-        navigateToAssayLandingPage();  //Should not cause unload warning
+        navigateToAssayLandingPage(SignalDataInitializer.RAW_SignalData_ASSAY);  //Should not cause unload warning
 
         // test upload of metadata file with full data file paths
-        importRun("importTest2",
+        importRun(SignalDataInitializer.RAW_SignalData_ASSAY,
+                "importTest2",
             getFile("RunsMetadata/datafiles2.tsv"),
             Collections.emptyList(),
             List.of(
@@ -164,7 +172,8 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
             ), Collections.EMPTY_MAP, 4);
 
         // test import of files with a subset of the metadata files
-        importRun("importTest3",
+        importRun(SignalDataInitializer.RAW_SignalData_ASSAY,
+                "importTest3",
                 getFile("RunsMetadata/datafiles.tsv"),
                 List.of(getFile(String.join("/", ASSAY_DATA_LOC, "BLANK235.TXT"))),
                 List.of(
@@ -173,7 +182,8 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
                 ),
                 expectedData, 3);
 
-        importRun("importTest4",
+        importRun(SignalDataInitializer.RAW_SignalData_ASSAY,
+                "importTest4",
                 getFile("RunsMetadata/datafiles2.tsv"),
                 Collections.emptyList(),
                 List.of(
@@ -182,16 +192,123 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
                 ), Collections.EMPTY_MAP, 4);
     }
 
+    @Test
+    public void testErrorConditions() throws IOException
+    {
+
+        String errorAssay = "Test Errors Conditions";
+
+        goToProjectHome();
+
+        log("Defining Error Assay");
+        goToManageAssays();
+
+        // We don't handle tricky characters well.
+        // Uncomment these lines once Issue 53965 is fixed.
+//        String strFieldName = TestDataGenerator.randomFieldName("Str");
+//        String intFieldName = TestDataGenerator.randomFieldName("Int");
+        String strFieldName = "Str";
+        String intFieldName = "Int";
+
+        FieldDefinition strField = new FieldDefinition(strFieldName, FieldDefinition.ColumnType.String)
+                .setRequired(true);
+
+        FieldDefinition intField = new FieldDefinition(intFieldName, FieldDefinition.ColumnType.Integer)
+                .setValidators(List.of(new FieldDefinition.RangeValidator("Large", "Must be greater than 5.",
+                        "Value must be greater than 5.",
+                        FieldDefinition.RangeType.GT, "5")));
+
+        ReactAssayDesignerPage assayDesigner = _assayHelper.createAssayDesign("Signal Data", errorAssay);
+        assayDesigner.setDescription("Testing error condition are handled correctly.");
+        assayDesigner.setEditableRuns(true);
+        assayDesigner.setEditableResults(true);
+        assayDesigner.goToResultsFields()
+                .addField(strField)
+                .addField(intField);
+        assayDesigner.clickFinish();
+
+        List<List<String>> fileData = new ArrayList<>();
+        fileData.add(List.of("Name", "DataFile", strField.getName(), intField.getName()));
+        fileData.add(List.of("Missing Required", RESULT_FILENAME_1, "", "123"));
+        fileData.add(List.of("Has All", RESULT_FILENAME_2, "DEF", "456"));
+        File metadataFile = TestDataUtils.writeRowsToTsv("Missing Require Result Field.tsv", fileData);
+
+        uploadWithErrorAction(errorAssay,
+                metadataFile,
+                "Missing Required Run",
+                String.format("Missing value for required property: %s", strField.getName()));
+
+        fileData = new ArrayList<>();
+        fileData.add(List.of("Name", "DataFile", strField.getName(), intField.getName()));
+        fileData.add(List.of("Valid Entry", RESULT_FILENAME_1, "ABC", "123"));
+        fileData.add(List.of("Incompatible Data Type", RESULT_FILENAME_2, "DEF", "GHI"));
+        metadataFile = TestDataUtils.writeRowsToTsv("Invalid Data Type.tsv", fileData);
+
+        uploadWithErrorAction(errorAssay,
+                metadataFile,
+                "Invalid Data Type",
+                String.format("Int: Value 'GHI' for field '%s' is invalid. Value must be greater than 5.", intField.getName()));
+
+        fileData = new ArrayList<>();
+        fileData.add(List.of("Name", "DataFile", strField.getName(), intField.getName()));
+        fileData.add(List.of("Valid Entry Again", RESULT_FILENAME_1, "ABC", "123"));
+        fileData.add(List.of("Range Validation Error", RESULT_FILENAME_2, "DEF", "2"));
+        metadataFile = TestDataUtils.writeRowsToTsv("Range Validation Error.tsv", fileData);
+
+        uploadWithErrorAction(errorAssay,
+                metadataFile,
+                "Invalid Data Type",
+                String.format("Int: Value '2' for field '%s' is invalid. Value must be greater than 5.", intField.getName()));
+
+    }
+
+    private void uploadWithErrorAction(String assayName,
+                                          File metadataFile,
+                                          String runId, String expectedMsg)
+    {
+
+        // If there is ever a desire to expand the error testing to include errors in the data files, then this list of
+        // data files should be identified in the test and passed in as a parameter.
+        List<File> dataFiles = List.of(
+                getFile(String.join("/", ASSAY_DATA_LOC, RESULT_FILENAME_1)),
+                getFile(String.join("/", ASSAY_DATA_LOC, RESULT_FILENAME_2)));
+
+        SignalDataUploadPage uploadPage = navigateToAssayLandingPage(assayName).navigateToImportPage();
+
+        log(String.format("Uploading metadata file: %s", metadataFile.getName()));
+        uploadPage.uploadMetadataFile(metadataFile);
+
+        log("Uploading data files.");
+        int uploadCount = dataFiles.size();
+        uploadPage.uploadFile(dataFiles);
+        uploadPage.waitForProgressBars(uploadCount);
+
+        uploadPage.setRunIDField(runId);
+        Window dialog = uploadPage.saveRunExpectingError(getDriver());
+
+        String actualMsg = dialog.getBody();
+
+        if (checker().withScreenshot()
+                .verifyTrue(String.format("Error dialog message '%s' does not contain expected message: %s", actualMsg, expectedMsg),
+                        actualMsg.contains(expectedMsg)))
+        {
+            dialog.clickButton("OK", true);
+            uploadPage.clearRun();
+        }
+
+    }
+
     private SignalDataAssayBeginPage importRun(
-        String runName,
-        File metadataFile,
-        List<File> unspecifiedDataFiles,
-        List<File> dataFiles,
-        Map<String, List<String>> expectedData,
-        int expectedResultRows
+            String assayName,
+            String runName,
+            File metadataFile,
+            List<File> unspecifiedDataFiles,
+            List<File> dataFiles,
+            Map<String, List<String>> expectedData,
+            int expectedResultRows
     )
     {
-        SignalDataAssayBeginPage beginPage = navigateToAssayLandingPage();
+        SignalDataAssayBeginPage beginPage = navigateToAssayLandingPage(assayName);
         SignalDataUploadPage uploadPage = beginPage.navigateToImportPage();
 
         log("Uploading metadata file");
@@ -236,11 +353,11 @@ public class SignalDataRawTest extends BaseWebDriverTest implements PostgresOnly
         return file;
     }
 
-    private SignalDataAssayBeginPage navigateToAssayLandingPage()
+    private SignalDataAssayBeginPage navigateToAssayLandingPage(String assayName)
     {
         //Navigate to Landing Page
         goToProjectHome();
-        clickAndWait(Locator.linkWithText(SignalDataInitializer.RAW_SignalData_ASSAY));
+        clickAndWait(Locator.linkWithText(assayName));
         SignalDataAssayBeginPage page = new SignalDataAssayBeginPage(this);
         page.waitForPageLoad();
         return page;
