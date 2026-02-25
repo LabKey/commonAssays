ed3da9b2eb0aae7803762f5c867a5832b841fd22
cnathe <cnathe@labkey.com>
2026-02-25 09:47:06 -0600

    GitHub Issue #875: LuminexMultipleCurvesTest test case update to verify total row count after multi file run import

--- luminex/test/src/org/labkey/test/tests/luminex/LuminexTest.java
+++ luminex/test/src/org/labkey/test/tests/luminex/LuminexTest.java
@@ -103,6 +103,7 @@ public abstract class LuminexTest extends BaseWebDriverTest
 
     public static final File RTRANSFORM_SCRIPT_FILE_LABKEY = new File(TestFileUtils.getLabKeyRoot(), "server/modules/commonAssays/luminex/resources/transformscripts/labkey_luminex_transform.R");
     public static final File RTRANSFORM_SCRIPT_FILE_LAB =  new File(TestFileUtils.getLabKeyRoot(), "server/modules/commonAssays/luminex/resources/transformscripts/tomaras_luminex_transform.R");
+    public static final File RTRANSFORM_SCRIPT_FILE_NOOP = TestFileUtils.getSampleData("qc/noopTransform.R");
 
     public static final String ASSAY_DATA_FILE_LOCATION_MULTIPLE_FIELD = "__primaryFile__";
 
