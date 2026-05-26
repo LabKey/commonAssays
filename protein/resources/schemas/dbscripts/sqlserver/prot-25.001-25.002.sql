92b869036d860811c03e1301f5c56a26d55c365e
Adam Rauch <adam@labkey.com>
2026-05-25 19:48:12 -0700

    Update copyrights and licenses (#1015)

--- protein/resources/schemas/dbscripts/sqlserver/prot-25.001-25.002.sql
+++ protein/resources/schemas/dbscripts/sqlserver/prot-25.001-25.002.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025-2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 -- Move foreign key to the correct table. Issue 53523.
 ALTER TABLE prot.Organisms DROP CONSTRAINT FK_ProtSequences_ProtOrganisms;
 ALTER TABLE prot.Sequences ADD CONSTRAINT FK_ProtSequences_ProtOrganisms FOREIGN KEY (OrgId) REFERENCES prot.Organisms (OrgId);
